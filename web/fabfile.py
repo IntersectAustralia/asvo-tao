dfa09a278057b0b91a73a04fb33808e17195a8fe
Ryan Braganza <ryan.braganza@gmail.com>
2012-09-05 17:21:58 +1000

    ASVO-18 fabfile

--- web/fabfile.py
+++ web/fabfile.py
@@ -1,35 +1,64 @@
 """
     Notes:
     * you may want to copy your ssh public keys to the remote hosts (not a task here)
+    * you may need to disable selinux
+    * assumes deploying to the user's home directory
+    * probably want to setup a root password on your mysql server
 """
 from fabric.api import *
 from fabric.context_managers import cd
 
 env.forward_agent = True
 
-# defaults
-
-env.user = 'devel'
-env.hosts = ['asvo-qa.intersect.org.au']
-
-
 # environments
 
-def qa(): # for completeness
+def qa():
     env.user = 'devel'
     env.hosts = ['asvo-qa.intersect.org.au']
+    env.target_env = 'qa'
+
 
 def demo():
     env.user = 'devel'
     env.hosts = ['asvo-demo.intersect.org.au']
+    env.target_env = 'demo'
+
+
+# defaults to QA settings
+qa()
 
 
 # tasks
 
+def _create_mysql_user_and_database():
+    # TODO don't hard-code username/password/database name
+    run("""echo "create user 'tao'@'localhost' identified by 'tao'; grant all privileges on tao.* to 'tao'@'localhost'; flush privileges;" | mysql -uroot """)
+    run("""echo "create database tao;" | mysql -utao --password=tao""")
+
 def initial_deploy():
-    sudo("yum install git -y")
+    sudo("yum install -y git mod_fcgid mysql-server mysql-devel gcc python-devel")
+    sudo("chkconfig mysqld on")
+    sudo("chkconfig httpd on")
+    sudo("service mysqld start")
+    _create_mysql_user_and_database()
     run("git clone git@github.com:IntersectAustralia/asvo-tao.git")
+    run("chmod o+rx /home/{user}".format(user=env.user))
+    with cd("asvo-tao/web"):
+        run("/usr/bin/env python2.6 bootstrap.py")
+        run("bin/buildout -c buildout_{target_env}.cfg".format(target_env=env.target_env))
+        run("bin/django collectstatic --noinput")
+        run("bin/django syncdb --noinput")
+        run("bin/django migrate")
+        sudo("cp deploy/httpd/tao_{target_env}_httpd.conf /etc/httpd/conf.d/tao_httpd.conf".format(target_env=env.target_env))
+        sudo("service httpd restart")
+        sudo("service httpd start")
+
 
 def update():
     with cd("asvo-tao/web"):
         run("git pull")
+        run("bin/buildout -c buildout_{target_env}.cfg".format(target_env=env.target_env))
+        run("bin/django collectstatic --noinput")
+        run("bin/django migrate")
+        sudo("cp deploy/httpd/tao_{target_env}_httpd.conf /etc/httpd/conf.d/tao_httpd.conf".format(target_env=env.target_env))
+        sudo("service httpd restart")
