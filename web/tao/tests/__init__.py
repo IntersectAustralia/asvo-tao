036633efc16e803dd8fc3a49ffb998c423a2f667
Ryan Braganza <ryan.braganza@gmail.com>
2012-09-07 16:50:26 +1000

    add pagination test

--- web/tao/tests/__init__.py
+++ web/tao/tests/__init__.py
@@ -1,3 +1,4 @@
-from create_user import *
-from forms.create_user_form import *
-from register_user import *
+from .create_user import *
+from .forms.create_user_form import *
+from .register_user import *
+from .pagination_test import *
