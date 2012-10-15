cc82976cefac491e478f714bd281f1ff17f33edd
amraziz8 <ahassan@swin.edu.au>
2012-10-15 17:01:39 +1100

    - Enable TABLESPACE and FILEGROUP - There is still error 4012 - Checking it ...

--- core/sageimport/MySQlDBInterface.py
+++ core/sageimport/MySQlDBInterface.py
@@ -29,13 +29,16 @@ class MySQlDBInterface(object):
         Constructor
         '''
         self.Options=Options
+        self.Log = open(self.Options['RunningSettings:OutputDir']+'DBCreation_sql.txt', 'wt')
+        
         self.CurrentSAGEStruct=CurrentSAGEStruct
+        
+        self.InitMySQLConnection()
         self.CreateNewTableTemplate()
         self.CreateInsertTemplate()
-        self.InitMySQLConnection()        
+                
         self.CreateDB()
-        if self.DebugToFile==True:
-            self.Log = open(self.Options['RunningSettings:OutputDir']+'Debug_sql.txt', 'wt')
+        
         self.CurrentGalaxiesCounter=int(Options['RunningSettings:GalaxiesPerTable'])+1 # To Create the First Table
     
     def CreateNewTableTemplate(self):
@@ -45,16 +48,15 @@ class MySQlDBInterface(object):
                 FieldDT=self.FormatMapping[field[1]]
                 FieldName=field[2]
                 self.CreateTableTemplate=self.CreateTableTemplate+ FieldName +' '+FieldDT+","
-        #self.CreateTableTemplate=self.CreateTableTemplate[:-1]
         self.CreateTableTemplate=self.CreateTableTemplate+"GlobalTreeID BIGINT,"
         self.CreateTableTemplate=self.CreateTableTemplate+"CentralGalaxyGlobalID BIGINT,"     
-        #self.CreateTableTemplate=self.CreateTableTemplate+"DescendantGlobalID BIGINT,"
         self.CreateTableTemplate=self.CreateTableTemplate+"LocalGalaxyID INT,"
         self.CreateTableTemplate=self.CreateTableTemplate+"CentralGalaxyX FLOAT,"
         self.CreateTableTemplate=self.CreateTableTemplate+"CentralGalaxyY FLOAT,"
         self.CreateTableTemplate=self.CreateTableTemplate+"CentralGalaxyZ FLOAT"
-        self.CreateTableTemplate=self.CreateTableTemplate+ ") ENGINE=NDBCLUSTER MAX_ROWS="+str(int(self.Options['RunningSettings:GalaxiesPerTable'])*5)     
-            
+        #self.CreateTableTemplate=self.CreateTableTemplate+ "PRIMARY KEY @TABLEName_PK (GlobalIndex)) "     
+        self.CreateTableTemplate=self.CreateTableTemplate+ " TABLESPACE "+self.DBName+"_TS STORAGE DISK "
+        self.CreateTableTemplate=self.CreateTableTemplate+" ENGINE=NDBCLUSTER MAX_ROWS="+str(int(self.Options['RunningSettings:GalaxiesPerTable'])*5)    
         
     
     
@@ -66,7 +68,6 @@ class MySQlDBInterface(object):
                 self.INSERTTemplate=self.INSERTTemplate+ FieldName+","
         self.INSERTTemplate=self.INSERTTemplate+"GlobalTreeID," 
         self.INSERTTemplate=self.INSERTTemplate+"CentralGalaxyGlobalID,"
-        #self.INSERTTemplate=self.INSERTTemplate+"DescendantGlobalID,"
         self.INSERTTemplate=self.INSERTTemplate+"LocalGalaxyID,"
         self.INSERTTemplate=self.INSERTTemplate+"CentralGalaxyX,"
         self.INSERTTemplate=self.INSERTTemplate+"CentralGalaxyY,"
@@ -86,29 +87,55 @@ class MySQlDBInterface(object):
         self.CurrentConnection=MySQLdb.connect(host=self.serverip,user=self.username,passwd=self.password)
         print('Connection to DB is open...')
         self.cursor=self.CurrentConnection.cursor()
-        self.cursor.execute("SELECT VERSION()")
+        self.ExecuteNoQuerySQLStatment("SELECT VERSION()")
         data=self.cursor.fetchone()        
         print ("Current MYSQL DB Version : "+data[0])
 
     def DropDatabase(self):
         ## Check if the database already exists
         self.cursor=self.CurrentConnection.cursor()
-        self.cursor.execute("Show databases Like '"+self.DBName+"'")
+        self.ExecuteNoQuerySQLStatment("Show databases Like '"+self.DBName+"'")
         data=self.cursor.fetchone()  
         ## If the database already exists - give the user the option to drop it
         if data!=None:
             Response=raw_input("Database "+self.DBName+" with the same name already exists!\nIf you Choose to Continue it will be dropped. Do you want to Drop it?(y/n)")
             if Response=='y':
                 ## Drop the database
-                self.cursor.execute("Drop database "+self.DBName+";")
+                self.ExecuteNoQuerySQLStatment("Drop database "+self.DBName+";")
+                #self.ExecuteNoQuerySQLStatment("Drop TABLESPACE "+self.DBName+"_LOGFILEGROUP ENGINE NDBCLUSTER;")
+                #self.ExecuteNoQuerySQLStatment("Drop LOGFILE "+self.DBName+"_TS ENGINE NDBCLUSTER;")
                 print("Database "+self.DBName+" Dropped")
+    
                 
+    def CreateTableSpace(self):
+        ####### Create a Log File Group ###################
+        ###### Table space allow MYSQL to store Non-Index Columns into the secondary storage instead of Memory #####
+        ####### See http://dev.mysql.com/doc/refman/5.1/en/mysql-cluster-disk-data.html for Details #####
+        
+        
+        CreateLogFileStatement="CREATE LOGFILE GROUP "+self.DBName+"_LOGFILEGROUP "
+        CreateLogFileStatement=CreateLogFileStatement+" ADD UNDOFILE \'"+self.DBName+"_LOGFILEGROUP_1"+".log\'"
+        CreateLogFileStatement=CreateLogFileStatement+" INITIAL_SIZE 128M"
+        CreateLogFileStatement=CreateLogFileStatement+" UNDO_BUFFER_SIZE 8M"
+        CreateLogFileStatement=CreateLogFileStatement+" ENGINE NDBCLUSTER;"
+        self.ExecuteNoQuerySQLStatment(CreateLogFileStatement);
+        
+        
+        CreateTableSapceFileStatement="CREATE TABLESPACE "+self.DBName+"_TS ";
+        CreateTableSapceFileStatement=CreateTableSapceFileStatement+" ADD DATAFILE \'"+self.DBName+"_TS_1"+".dat\'"
+        CreateTableSapceFileStatement=CreateTableSapceFileStatement+" USE LOGFILE GROUP "+self.DBName+"_LOGFILEGROUP "
+        CreateTableSapceFileStatement=CreateTableSapceFileStatement+" INITIAL_SIZE 128M"
+        CreateTableSapceFileStatement=CreateTableSapceFileStatement+" ENGINE NDBCLUSTER;"
+        self.ExecuteNoQuerySQLStatment(CreateTableSapceFileStatement);
+        
+        
         
     def CreateDB(self):
        ## Check if the database already exist and give the user the option to Drop it 
-       self.DropDatabase() 
+       self.DropDatabase()        
+       #self.CreateTableSpace()
        ## Create New DB 
-       self.cursor.execute("create database "+self.DBName+";") 
+       self.ExecuteNoQuerySQLStatment("create database "+self.DBName+";") 
        print("Database "+self.DBName+" Created")
        ### Close the current Connection and open a new one on the new DB
        self.CurrentConnection.close()      
@@ -116,29 +143,42 @@ class MySQlDBInterface(object):
        self.cursor=self.CurrentConnection.cursor()
        print("Connection to Database "+self.DBName+" is opened and ready")
    
-    def CreateNewTable(self,TableIndex):
+    def ExecuteNoQuerySQLStatment(self,SQLStatment):
+        if self.DebugToFile==True:
+            self.Log.write(SQLStatment+"\n\n")
+            self.Log.flush()
+        self.cursor.execute(SQLStatment)
+        self.CurrentConnection.commit()  
+    
+    def CreateNewTable(self,TableIndex):       
+        
+            
         TablePrefix=self.Options['MySQLDB:TreeTablePrefix']
         NewTableName=TablePrefix+str(TableIndex)
-        CreateTableStatment= string.replace(self.CreateTableTemplate,"@TABLEName",NewTableName)        
+        CreateTableStatment= string.replace(self.CreateTableTemplate,"@TABLEName",NewTableName)
+        if self.DebugToFile==True:
+            if self.Log!=None:
+                self.Log.close()
+            self.Log = open(self.Options['RunningSettings:OutputDir']+NewTableName+'_sql.txt', 'wt')
+                    
         #print CreateTableStatment
-        self.cursor.execute(CreateTableStatment)
+        self.ExecuteNoQuerySQLStatment(CreateTableStatment)
         
         
         CreateIndexStatment="Create Index GlobalIndex_Index on  "+NewTableName+" (GlobalIndex);"
-        self.cursor.execute(CreateIndexStatment)
+        self.ExecuteNoQuerySQLStatment(CreateIndexStatment)
         CreateIndexStatment="Create Index SnapNum_Index on  "+NewTableName+" (SnapNum);"
-        self.cursor.execute(CreateIndexStatment)
+        self.ExecuteNoQuerySQLStatment(CreateIndexStatment)
         CreateIndexStatment="Create Index GlobalTreeID_Index on  "+NewTableName+" (GlobalTreeID);"
-        self.cursor.execute(CreateIndexStatment)
+        self.ExecuteNoQuerySQLStatment(CreateIndexStatment)
         CreateIndexStatment="Create Index CentralGalaxyX_Index on  "+NewTableName+" (CentralGalaxyX);"
-        self.cursor.execute(CreateIndexStatment)
+        self.ExecuteNoQuerySQLStatment(CreateIndexStatment)
         CreateIndexStatment="Create Index CentralGalaxyY_Index on  "+NewTableName+" (CentralGalaxyY);"
-        self.cursor.execute(CreateIndexStatment)
+        self.ExecuteNoQuerySQLStatment(CreateIndexStatment)
         CreateIndexStatment="Create Index CentralGalaxyZ_Index on  "+NewTableName+" (CentralGalaxyZ);"
-        self.cursor.execute(CreateIndexStatment)
-        #CreatePartitionStatment="ALTER TABLE "+NewTableName+" PARTITION BY KEY(GlobalTreeID) PARTITIONS 2;"    
-        #self.cursor.execute(CreatePartitionStatment)
-        self.CurrentConnection.commit()   
+        self.ExecuteNoQuerySQLStatment(CreateIndexStatment)
+        
+         
         print("Table "+NewTableName+" Created With Index ...")
     
           
@@ -197,11 +237,11 @@ class MySQlDBInterface(object):
             self.Log.write(InsertStatment+"\n\n")
             self.Log.flush()
         #print(InsertStatment)    
-        self.cursor.execute(InsertStatment)
-        self.CurrentConnection.commit()     
+        self.ExecuteNoQuerySQLStatment(InsertStatment)
+            
     def Close(self):
         self.CurrentConnection.close()
-        if self.DebugToFile==True:
+        if self.DebugToFile==True and self.Log!=None:
             self.Log.close()
             
         
