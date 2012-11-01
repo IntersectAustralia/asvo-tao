6d1eb20491b58c4c79a7e04105d022a57798bf8a
Ryan Braganza <ryan.braganza@gmail.com>
2012-11-01 17:11:21 +1100

    update production settings

--- web/tao/production.py
+++ web/tao/production.py
@@ -1,8 +1,29 @@
-
 from tao.settings import *
 
+DEBUG=False
+TEMPLATE_DEBUG=DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'OPTIONS': {
+            'init_command': 'SET storage_engine=INNODB, SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED'
+        }
+    }
+}
+
 SECRET_KEY = ''
-ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
-)
-MANAGERS = ADMINS
+from tao.secrets import *
+
+STATIC_URL = '/taodemo/static/'
+FILES_BASE = '/mnt/TAOAdmin/'  # please include a trailing slash
+
+EMAIL_HOST = 'gpo.swin.edu.au'
+EMAIL_PORT = '25'
+
+EMAIL_FROM_ADDRESS = 'tao.admin@asvo.org.au'
