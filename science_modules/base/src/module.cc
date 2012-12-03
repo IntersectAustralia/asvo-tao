620f48d5787ac816ff2b9d06f58b5b799948a448
Luke Hodkinson <furious.luke@gmail.com>
2012-12-04 08:26:26 +1100

    Updating database connection to use postgresql.

--- science_modules/base/src/module.cc
+++ science_modules/base/src/module.cc
@@ -1,4 +1,5 @@
 #include <soci/sqlite3/soci-sqlite3.h>
+#include <soci/postgresql/soci-postgresql.h>
 #include "module.hh"
 
 using namespace hpc;
@@ -11,27 +12,30 @@ namespace tao {
    }
 
    void
-   module::_db_connect( soci::session& sql,
-                        const string& type,
-                        const string& name )
+   module::_db_connect( soci::session& sql )
    {
       LOG_ENTER();
 
-      LOGLN( "Connecting to ", type, " database: ", name );
+      LOGDLN( "Connecting to ", _dbtype, " database \"", _dbname, "\"" );
       try
       {
-         if( type == "sqlite" )
-            sql.open( soci::sqlite3, name );
+         if( _dbtype == "sqlite" )
+            sql.open( soci::sqlite3, _dbname );
          else
          {
-            ASSERT( 0 );
-            // sql.open( soci::mysql, str( format( "host=%1% db=%2% user=%3% password='%4'" ) % _dbhost % _dbname % _dbuser % _dbpass ) );
+	    string connect = "dbname=" + _dbname;
+	    connect += " host=" + _dbhost;
+	    connect += " port=" + _dbport;
+	    connect += " user=" + _dbuser;
+	    connect += " password='" + _dbpass + "'";
+	    LOGDLN( "Connect string: ", connect );
+	    sql.open( soci::postgresql, connect );
          }
       }
       catch( const std::exception& ex )
       {
          // TODO: Handle database errors.
-         LOGLN( "Error opening database connection: ", ex.what() );
+         LOGDLN( "Error opening database connection: ", ex.what() );
          ASSERT( 0 );
       }
 
@@ -46,7 +50,7 @@ namespace tao {
    {
       if( _connected )
       {
-         LOGLN( "Disconnecting from database." );
+         LOGDLN( "Disconnecting from database." );
          _sql.close();
          _connected = false;
       }
