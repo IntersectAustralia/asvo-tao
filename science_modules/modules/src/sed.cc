9ef22cfb7de12d3fd0405219b4e6a7fdccc20f98
Luke Hodkinson <furious.luke@gmail.com>
2012-12-06 12:28:30 +1100

    Updating the star formation history rebinning code. Now it's all in there, but we may need to confirm the units (it'll be pretty obvious if the masses are out by a huge factor).

--- science_modules/modules/src/sed.cc
+++ science_modules/modules/src/sed.cc
@@ -11,7 +11,6 @@ using boost::str;
 namespace tao {
 
    sed::sed()
-      : _cur_flat_file( -1 )
    {
    }
 
@@ -29,11 +28,12 @@ namespace tao {
    sed::setup_options( options::dictionary& dict,
                        optional<const string&> prefix )
    {
-      // dict.add_option( new options::string( "database_type" ), prefix );
-      // dict.add_option( new options::string( "database_name" ), prefix );
-      // dict.add_option( new options::string( "database_host", string() ), prefix );
-      // dict.add_option( new options::string( "database_user", string() ), prefix );
-      // dict.add_option( new options::string( "database_pass", string() ), prefix );
+      dict.add_option( new options::string( "database_type" ), prefix );
+      dict.add_option( new options::string( "database_name" ), prefix );
+      dict.add_option( new options::string( "database_host", string() ), prefix );
+      dict.add_option( new options::string( "database_port", string() ), prefix );
+      dict.add_option( new options::string( "database_user", string() ), prefix );
+      dict.add_option( new options::string( "database_pass", string() ), prefix );
       dict.add_option( new options::string( "ssp_filename" ), prefix );
       dict.add_option( new options::integer( "num_spectra" ), prefix );
       dict.add_option( new options::integer( "num_metals" ), prefix );
@@ -57,9 +57,6 @@ namespace tao {
                     optional<const string&> prefix )
    {
       LOG_ENTER();
-
-      // Create flat HDF5 types.
-      make_hdf5_types<real_type>( _flat_mem_type, _flat_file_type );
 
       _read_options( dict, prefix );
 
@@ -105,7 +102,7 @@ namespace tao {
       LOGLN( "Processing galaxy with ID ", gal_id );
 
       // Read the star-formation histories for this galaxy.
-      _rebin_info( galaxy.flat_file(), galaxy.flat_offset(), galaxy.flat_length() );
+      _rebin_info( galaxy );
 
       // Clear disk and bulge output spectrums.
       std::fill( _disk_spectra.begin(), _disk_spectra.end(), 0.0 );
@@ -203,156 +200,100 @@ namespace tao {
    }
 
    void
-   sed::_rebin_info( unsigned flat_file,
-                     unsigned flat_offset,
-                     unsigned flat_length )
+   sed::_rebin_info( const tao::galaxy& galaxy )
    {
       LOG_ENTER();
 
-      // Be sure we have the correct flat information available.
-      _update_flat_info( flat_file );
+      // Clear out values.
+      std::fill( _disk_age_masses.begin(), _disk_age_masses.end(), 0.0 );
+      std::fill( _bulge_age_masses.begin(), _bulge_age_masses.end(), 0.0 );
+      std::fill( _disk_age_metals.begin(), _disk_age_metals.end(), 0.0 );
+      std::fill( _bulge_age_metals.begin(), _bulge_age_metals.end(), 0.0 );
 
-      // If there is only one item to rebin, don't try to build the cubic splines.
-      if( flat_length > 1 )
-      {
-         LOGLN( "Flat length greater than one." );
-
-         // Create the knots for disk/bulge star formation rates.
-         fibre<real_type> dmassknots( 2, flat_length ), bmassknots( 2, flat_length );
-         fibre<real_type> dmetknots( 2, flat_length ), bmetknots( 2, flat_length );
-         for( unsigned ii = 0; ii < flat_length; ++ii )
-         {
-            dmassknots(ii,0) = _flat_data[flat_offset + ii].redshift;
-            bmassknots(ii,0) = _flat_data[flat_offset + ii].redshift;
-            dmetknots(ii,0) = _flat_data[flat_offset + ii].redshift;
-            bmetknots(ii,0) = _flat_data[flat_offset + ii].redshift;
-            dmassknots(ii,1) = _flat_data[flat_offset + ii].disk_rate;
-            bmassknots(ii,1) = _flat_data[flat_offset + ii].bulge_rate;
-            dmetknots(ii,1) = _flat_data[flat_offset + ii].disk_metal;
-            bmetknots(ii,1) = _flat_data[flat_offset + ii].bulge_metal;
-         }
-         LOGLN( "Disk knots: ", dmassknots );
-         LOGLN( "Bulge knots: ", bmassknots );
-
-         // Prepare splines.
-         numerics::spline<real_type> dmassspline, bmassspline;
-         numerics::spline<real_type> dmetspline, bmetspline;
-         dmassspline.set_knots( dmassknots );
-         bmassspline.set_knots( bmassknots );
-         dmetspline.set_knots( dmetknots );
-         bmetspline.set_knots( bmetknots );
-
-         // Clear out values.
-         std::fill( _disk_age_masses.begin(), _disk_age_masses.end(), 0.0 );
-         std::fill( _bulge_age_masses.begin(), _bulge_age_masses.end(), 0.0 );
-         std::fill( _disk_age_metals.begin(), _disk_age_metals.end(), 0.0 );
-         std::fill( _bulge_age_metals.begin(), _bulge_age_metals.end(), 0.0 );
-
-         // Integrate.
-         typedef vector<real_type>::view array_type;
-         element<array_type> take_first( 0 );
-         range<real_type> old_rng( dmassspline.knots().front()[0], dmassspline.knots().back()[0] );
-         range<real_type> new_rng( _ages.front(), _ages.back() );
-         LOGLN( "Age range: ", new_rng );
-         LOGLN( "Galaxy redshift range: ", old_rng );
-         real_type low = old_rng.start();
-         real_type upp = old_rng.finish();
-         auto it = make_interp_iterator(
-            boost::make_transform_iterator( dmassspline.knots().begin(), take_first ),
-            boost::make_transform_iterator( dmassspline.knots().end(), take_first ),
-            _ages.begin(),
-            _ages.end(),
-            1e-7
-            );
-         while( !num::approx( *it, low, 1e-7 ) )
-            ++it;
-         vector<real_type> crds( 4 ), weights( 4 );
-         _gauss_quad( crds, weights );
-         while( !num::approx( *it++, upp, 1e-7 ) )
-         {
-            LOGLN( "Integrating range: (", low, ", ", *it, ")" );
-            real_type w = *it - low;
-            real_type jac_det = 0.5*w;
-            unsigned old_poly = it.indices()[0] - 1;
-            unsigned new_poly = it.indices()[1] - 1;
-            LOGLN( "Galaxy/age indices: ", old_poly, ", ", new_poly );
-            for( unsigned ii = 0; ii < 4; ++ii )
-            {
-               real_type x = low + w*0.5*(1.0 + crds[ii]);
-
-               // This integral looks like this because of a change of variable
-               // frome wavelength to frequency. Do the math!
-               _disk_age_masses[new_poly] += jac_det*weights[ii]*dmassspline( x, old_poly )*1e10;
-               _bulge_age_masses[new_poly] += jac_det*weights[ii]*bmassspline( x, old_poly )*1e10;
-            }
-
-            // Interpolate metallicity.
-            _disk_age_metals[new_poly] = dmetspline( low, old_poly );
-            _bulge_age_metals[new_poly] = bmetspline( low, old_poly );
-
-            // Advance.
-            low = *it;
-         }
-      }
-      else
-      {
-         LOGLN( "Flat length equal to one." );
-
-         // Locate the correct bin.
-         auto it = std::lower_bound( _ages.begin(), _ages.end(), _flat_data[flat_offset].redshift );
-         unsigned bin;
-         if( it == _ages.end() )
-            bin = _ages.size() - 1;
-         else
-            bin = *it;
-         LOGLN( "Redshift ", _flat_data[flat_offset].redshift, " correlates to bin ", bin );
-         _disk_age_masses[bin] = _flat_data[flat_offset].disk_mass;
-         _bulge_age_masses[bin] = _flat_data[flat_offset].bulge_mass;
-         _disk_age_metals[bin] = _flat_data[flat_offset].disk_metal;
-         _bulge_age_metals[bin] = _flat_data[flat_offset].bulge_metal;
-      }
-
-      LOGLN( "Disk masses: ", _disk_age_masses );
-      LOGLN( "Bulge masses: ", _bulge_age_masses );
-      LOGLN( "Disk metals: ", _disk_age_metals );
-      LOGLN( "Bulge metals: ", _bulge_age_metals );
-      LOG_EXIT();
+      // Rebin everything from this galaxy.
+      unsigned id = galaxy.local_id();
+      _rebin_parents( id, id );
    }
 
    void
-   sed::_gauss_quad( vector<real_type>::view crds,
-                     vector<real_type>::view weights )
+   sed::_rebin_parents( unsigned id,
+			unsigned root_id )
    {
-      real_type v0 = sqrt( (3.0 - 2.0*sqrt( 6.0/5.0 ))/7.0 );
-      real_type v1 = sqrt( (3.0 + 2.0*sqrt( 6.0/5.0 ))/7.0 );
-      crds[0] = -v1;
-      crds[1] = -v0;
-      crds[2] = v0;
-      crds[3] = v1;
-      weights[0] = (18.0 - sqrt( 30.0 ))/36.0;
-      weights[1] = (18.0 + sqrt( 30.0 ))/36.0;
-      weights[2] = (18.0 + sqrt( 30.0 ))/36.0;
-      weights[3] = (18.0 - sqrt( 30.0 ))/36.0;
+      // Find the bin for the galaxy.
+      unsigned last_bin = _find_bin( id );
+      real_type last_age = _calc_age( id, root_id );
+
+      // Get the range of parents.
+      auto rng = _parents.equal_range( id );
+      while( rng.first != rng.second )
+      {
+   	 // Cache the parent.
+   	 unsigned par = (*rng.first++).second;
+
+	 // Find the bin for this parent.
+	 unsigned first_bin = _find_bin( par );
+	 real_type first_age = _calc_age( par, root_id );
+
+	 // Is this real time section contained entirely within
+	 // one bin?
+	 if( first_bin == last_bin )
+	 {
+	    _update_bin( first_bin, par, last_age - first_age );
+	 }
+
+	 // Split across multiple bins.
+	 else
+	 {
+	    // Update the first bin portion.
+	    _update_bin( first_bin, par, _dual_ages[first_bin + 1] - first_age );
+
+	    // Update any full intermediate bins.
+	    while( ++first_bin < last_bin )
+	       _update_bin( first_bin, par, _dual_ages[first_bin + 1] - _dual_ages[first_bin] );
+
+	    // Update the last bin portion.
+	    _update_bin( last_bin, par, _dual_ages[last_bin] - last_age );
+	 }
+
+	 // Recursively process parents.
+	 _rebin_parents( par, root_id );
+      }
    }
 
    void
-   sed::_update_flat_info( unsigned flat_file )
+   sed::_update_bin( unsigned bin,
+		     unsigned id,
+		     real_type dt )
    {
-      LOG_ENTER();
+      real_type add_dm = (_sfrs[id] - _bulge_sfrs[id])*dt;
+      real_type add_bm = _bulge_sfrs[id]*dt;
+      real_type new_dm = _disk_age_masses[bin] + add_dm;
+      real_type new_bm = _bulge_age_masses[bin] + add_bm;
+      _disk_age_metals[bin] = (_disk_age_masses[bin]*_disk_age_metals[bin] + add_dm*(_metals[id] - _bulge_metals[id]))/new_dm;
+      _bulge_age_metals[bin] = (_bulge_age_masses[bin]*_bulge_age_metals[bin] + add_bm*_bulge_metals[id])/new_bm;
+      _disk_age_masses[bin] = new_dm;
+      _bulge_age_masses[bin] = new_bm;
+   }
 
-      if( flat_file != _cur_flat_file )
+   unsigned
+   sed::_find_bin( unsigned parent )
+   {
+      // Which bin should this parent belong in?
+      real_type redshift = _redshifts[parent];
+      unsigned bin;
       {
-         LOGLN( "Need to change flat file." );
-
-         // For now I force continually increasing flat files. This may
-         // change in the future.
-         ASSERT( (int)flat_file > _cur_flat_file );
-
-         _read_flat_file( "trees", flat_file );
-         _cur_flat_file = flat_file;
+	 auto it = std::lower_bound( _ages.begin(), _ages.end(), redshift );
+	 ASSERT( it != _ages.end() );
+	 bin = it - _ages.begin();
       }
+      return bin;
+   }
 
-      LOG_EXIT();
+   sed::real_type
+   sed::_calc_age( unsigned id,
+		   unsigned root_id )
+   {
+      return 0.0;
    }
 
    void
@@ -362,13 +303,14 @@ namespace tao {
       // Get the sub dictionary, if it exists.
       const options::dictionary& sub = prefix ? dict.sub( *prefix ) : dict;
 
-      // // Extract database details.
-      // _dbtype = sub.get<string>( "database_type" );
-      // _dbname = sub.get<string>( "database_name" );
-      // _dbhost = sub.get<string>( "database_host" );
-      // _dbuser = sub.get<string>( "database_user" );
-      // _dbpass = sub.get<string>( "database_pass" );
-      // _db_connect( _sql, _dbtype, _dbname );
+      // Extract database details.
+      _dbtype = sub.get<string>( "database_type" );
+      _dbname = sub.get<string>( "database_name" );
+      _dbhost = sub.get<string>( "database_host" );
+      _dbport = sub.get<string>( "database_port" );
+      _dbuser = sub.get<string>( "database_user" );
+      _dbpass = sub.get<string>( "database_pass" );
+      _db_connect( _sql );
 
       // Extract the counts.
       _num_spectra = sub.get<unsigned>( "num_spectra" );
@@ -405,10 +347,10 @@ namespace tao {
       // Must be ordered.
       std::sort( _ages.begin(), _ages.end() );
 
-      // Take the dual.
+      // Take the dual to form age bins.
+      _dual_ages.reallocate( _ages.size() - 1 );
       for( unsigned ii = 0; ii < _ages.size() - 1; ++ii )
          _ages[ii] = 0.5*(_ages[ii] + _ages[ii + 1]);
-      _ages.resize( _ages.size() - 1 );
 
       // Allocate. Note that the ordering goes time,spectra,metals.
       _ssp.reallocate( _ages.size()*_num_spectra*_num_metals );
@@ -426,12 +368,45 @@ namespace tao {
    }
 
    void
-   sed::_read_flat_file( const string& base_filename,
-                         unsigned flat_file )
+   sed::_load_table( const string& table )
    {
-      string filename = base_filename + string( ".flat." ) + to_string( flat_file );
-      h5::file file( filename, H5F_ACC_RDONLY );
-      _flat_data.reallocate( file.read_data_size( "flat_trees" ) );
-      file.read<flat_info<real_type>>( "flat_trees", _flat_mem_type, _flat_data );
+      // Clear away any existing data.
+      _descs.deallocate();
+      _sfrs.deallocate();
+      _bulge_sfrs.deallocate();
+      _metals.deallocate();
+      _bulge_metals.deallocate();
+      _redshifts.deallocate();
+      _parents.clear();
+
+      // First need to pull the table's tree id.
+      long long tree_id;
+      _sql << "SELECT globaltreeid FROM " + table, soci::into( tree_id );
+
+      // Extract number of records in this tree.
+      unsigned tree_size;
+      _sql << "SELECT galaxycount FROM treesummary WHERE globaltreeid = :id",
+   	 soci::into( tree_size ), soci::use( tree_id );
+
+      // Resize all our arrays.
+      _descs.resize( tree_size );
+      _sfrs.resize( tree_size );
+      _bulge_sfrs.resize( tree_size );
+      _metals.resize( tree_size );
+      _bulge_metals.resize( tree_size );
+      _redshifts.resize( tree_size );
+
+      // Extract the table.
+      string query = "SELECT globalindex, descendant, stellarmass, bulgemass, sfr, sfrbulge, "
+	 "metalsstellarmass, metalsbulgemass, redshift"
+   	 " INNER JOIN snap_redshift ON (" + table + ".snapnum = snap_redshift.snapnum)";
+      _sql << query, soci::into( (std::vector<int>&)_descs ),
+   	 soci::into( (std::vector<double>&)_sfrs ), soci::into( (std::vector<double>&)_bulge_sfrs ),
+	 soci::into( (std::vector<double>&)_metals ), soci::into( (std::vector<double>&)_bulge_metals ),
+	 soci::into( (std::vector<double>&)_redshifts );
+
+      // Build the parents for each galaxy.
+      for( unsigned ii = 0; ii < _descs.size(); ++ii )
+   	 _parents.insert( _descs[ii], ii );
    }
 }
