1e16e927cded9a3d63dd3e31c53167c12de05b90
Ryan Braganza <ryan.braganza@gmail.com>
2012-12-10 10:31:03 +1100

    Add Amr's IP to API allowed addresses

--- web/tao/production.py
+++ web/tao/production.py
@@ -29,7 +29,7 @@ EMAIL_PORT = '25'
 EMAIL_FROM_ADDRESS = 'tao.admin@asvo.org.au'
 
 API_ALLOWED_IPS = (
-                   '127.0.0.1',
                    '136.186.55.225', # tao01
                    '136.186.55.226', # tao02
+                   '136.186.24.174', # Amr
                    )
