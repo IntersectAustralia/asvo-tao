2e9f2b1c947e9261f90320dae85087bbe7ed800e
amraziz8 <ahassan@swin.edu.au>
2013-01-14 16:06:52 +1100

    change to lxml Add new element for the database

--- core/workflow/ParseXML.py
+++ core/workflow/ParseXML.py
@@ -1,5 +1,5 @@
 import re
-import xml.etree.ElementTree as ET
+import lxml.etree as ET
 
 
 
@@ -7,33 +7,57 @@ class ParseXMLParameters(object):
 
     
 
-    def __init__(self,FileName):
-        self.tree = ET.ElementTree(file=FileName)
-        self.NameSpace=re.findall('\{.*\}',self.tree.findall('.')[0].tag)[0]
+    def __init__(self,FileName):        
+        self.tree = ET.parse(FileName)
+        self.NameSpace=re.findall('\{.*\}',self.tree.xpath('.')[0].tag)[0]
+        self.NameSpace=self.NameSpace[1:-1]
     def ParseFile(self):
         self.GetCurrentUser()
         self.GetDocumentSignature()
-        self.FindModules()
+        #self.FindModules()
+        self.GetDatabase()
+        
+        
+        
+    def ExportTree(self,FileName): 
+        with open(FileName,'w') as f:
+            f.write(ET.tostring(self.tree,encoding='UTF-8',xml_declaration=True,pretty_print=True))       
+        
            
     def GetCurrentUser(self):
-        self.UserName=self.tree.findall(self.NameSpace+"username")[0].text
+        self.UserName=self.tree.xpath("ns:username",namespaces={'ns':self.NameSpace})[0].text
     def GetDocumentSignature(self):
-        self.Signature=self.tree.findall(self.NameSpace+"signature")[0].text
+        self.Signature=self.tree.xpath("ns:signature",namespaces={'ns':self.NameSpace})[0].text
         print self.Signature
     def FindModules(self):        
-        self.Modules=self.tree.findall(self.NameSpace+"workflow/"+self.NameSpace+"module")
+        self.Modules=self.tree.xpath("ns:workflow/ns:module",namespaces={'ns':self.NameSpace})
         for Module in self.Modules:
             print Module.attrib['name'] 
             print('----------------------------------') 
-            ModuleParams=Module.findall('*')
+            ModuleParams=Module.xpath('*')
             for Param in ModuleParams:
                 if Param.attrib.get('name') !=None:
                     print (Param.attrib['name']+":"+Param.text)
-            
+    def GetDatabase(self):
+        FModules=self.tree.xpath("ns:workflow/ns:module[@name='light-cone']",namespaces={'ns':self.NameSpace})
+        if len(FModules)>0:
+            self.LightConeModule=FModules[0]
+        else:
+            raise Exception('Error In Getting Database information','Light Cone module cannot be found!')
+        self.Simulation=self.LightConeModule.xpath("ns:*[@name='simulation']",namespaces={'ns':self.NameSpace})[0].text
+        self.GalaxyModel=self.LightConeModule.xpath("ns:*[@name='galaxymodel']",namespaces={'ns':self.NameSpace})[0].text
+        DBElement=ET.Element("param")
+        DBElement.attrib['name']='database'
+        DBElement.text=self.Simulation+":"+self.GalaxyModel
+        self.LightConeModule.append(DBElement)
+        
+        
+        
 if __name__ == '__main__':
     
     
     ParseXMLParametersObj=ParseXMLParameters('/home/amr/tao.xml')
     ParseXMLParametersObj.ParseFile()
+    ParseXMLParametersObj.ExportTree("/home/amr/tao01.xml")
 
     
