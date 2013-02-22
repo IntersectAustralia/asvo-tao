a868b9b7d5bdec3aea8b5ca7277a6737d4a99230
Luke Hodkinson <furious.luke@gmail.com>
2013-02-22 16:34:31 +1100

    Splitting out changes to galaxy.hh and galaxy.cc in order to merge it with the master.

--- science_modules/base/src/galaxy.cc
+++ science_modules/base/src/galaxy.cc
@@ -6,13 +6,33 @@ using namespace hpc;
 namespace tao {
 
    galaxy::galaxy( const soci::row& row,
-                   const array<real_type,3>& box,
 		   const string& table )
-      : _row( row ),
-        _box( box ),
-	_table( table )
+      : _row( NULL ),
+	_table( NULL )
    {
-      _z = _row.get<real_type>( "redshift" );
+      update( row, table );
+   }
+
+   galaxy::galaxy()
+      : _row( NULL ),
+        _table( NULL )
+   {
+   }
+
+   void
+   galaxy::update( const soci::row& row,
+                   const string& table )
+   {
+      _row = &row;
+      _table = &table;
+      _z = _row->get<real_type>( "redshift" );
+      _fields.clear();
+   }
+
+   void
+   galaxy::update( const galaxy& gal )
+   {
+      update( *gal._row, *gal._table );
    }
 
    void
@@ -24,84 +44,95 @@ namespace tao {
    const soci::row&
    galaxy::row() const
    {
-      return _row;
+      ASSERT( _row );
+      return *_row;
    }
 
    const string&
    galaxy::table() const
    {
-      return _table;
+      ASSERT( _table );
+      return *_table;
    }
 
    long long
    galaxy::id() const
    {
-      return _row.get<long long>( "globalindex" );
+      ASSERT( _row );
+      return _row->get<long long>( "globalindex" );
    }
 
    int
    galaxy::local_id() const
    {
-      return _row.get<int>( "localgalaxyid" );
+      ASSERT( _row );
+      return _row->get<int>( "localgalaxyid" );
    }
 
    long long
    galaxy::tree_id() const
    {
-      return _row.get<long long>( "globaltreeid" );
+      ASSERT( _row );
+      return _row->get<long long>( "globaltreeid" );
    }
 
    galaxy::real_type
    galaxy::x() const
    {
+      ASSERT( _row );
+
       // This is ridiculous, but it seems sqlite represents
       // modified aliases, (0 + posx + 0) AS pos_x, as strings.
       // "Surely not!?" you say, but indeed it is so. This
       // means sqlite databases won't have the correct rotated/
       // translated values used as pos_x, pos_y, pos_z.
 #ifndef NDEBUG
-      if( _row.get_properties( "pos_x" ).get_data_type() == soci::dt_string )
-	 return atof( _row.get<std::string>( "pos_x" ).c_str() );
+      if( _row->get_properties( "pos_x" ).get_data_type() == soci::dt_string )
+	 return atof( _row->get<std::string>( "pos_x" ).c_str() );
       else
-	 return _row.get<real_type>( "pos_x" );
+	 return _row->get<real_type>( "pos_x" );
 #else
-      return _row.get<real_type>( "pos_x" );
+      return _row->get<real_type>( "pos_x" );
 #endif
    }
 
    galaxy::real_type
    galaxy::y() const
    {
+      ASSERT( _row );
+
       // This is ridiculous, but it seems sqlite represents
       // modified aliases, (0 + posx + 0) AS pos_x, as strings.
       // "Surely not!?" you say, but indeed it is so. This
       // means sqlite databases won't have the correct rotated/
       // translated values used as pos_x, pos_y, pos_z.
 #ifndef NDEBUG
-      if( _row.get_properties( "pos_y" ).get_data_type() == soci::dt_string )
-	 return atof( _row.get<std::string>( "pos_y" ).c_str() );
+      if( _row->get_properties( "pos_y" ).get_data_type() == soci::dt_string )
+	 return atof( _row->get<std::string>( "pos_y" ).c_str() );
       else
-	 return _row.get<real_type>( "pos_y" );
+	 return _row->get<real_type>( "pos_y" );
 #else
-      return _row.get<real_type>( "pos_y" );
+      return _row->get<real_type>( "pos_y" );
 #endif
    }
 
    galaxy::real_type
    galaxy::z() const
    {
+      ASSERT( _row );
+
       // This is ridiculous, but it seems sqlite represents
       // modified aliases, (0 + posx + 0) AS pos_x, as strings.
       // "Surely not!?" you say, but indeed it is so. This
       // means sqlite databases won't have the correct rotated/
       // translated values used as pos_x, pos_y, pos_z.
 #ifndef NDEBUG
-      if( _row.get_properties( "pos_z" ).get_data_type() == soci::dt_string )
-	 return atof( _row.get<std::string>( "pos_z" ).c_str() );
+      if( _row->get_properties( "pos_z" ).get_data_type() == soci::dt_string )
+	 return atof( _row->get<std::string>( "pos_z" ).c_str() );
       else
-	 return _row.get<real_type>( "pos_z" );
+	 return _row->get<real_type>( "pos_z" );
 #else
-      return _row.get<real_type>( "pos_z" );
+      return _row->get<real_type>( "pos_z" );
 #endif
    }
 
@@ -114,31 +145,51 @@ namespace tao {
    galaxy::real_type
    galaxy::disk_metallicity() const
    {
-      return _row.get<real_type>( "disk_metal" );
+      ASSERT( _row );
+      return _row->get<real_type>( "disk_metal" );
    }
 
    galaxy::real_type
    galaxy::bulge_metallicity() const
    {
-      return _row.get<real_type>( "bulge_metal" );
+      ASSERT( _row );
+      return _row->get<real_type>( "bulge_metal" );
    }
 
-   unsigned
-   galaxy::flat_file() const
+   galaxy::field_type
+   galaxy::field( const string& name ) const
    {
-      return _row.get<int>( "flat_file" );
-   }
+      ASSERT( _row );
 
-   unsigned
-   galaxy::flat_offset() const
-   {
-      return _row.get<int>( "flat_offset" );
-   }
+      auto it = _fields.find( name );
 
-   unsigned
-   galaxy::flat_length() const
-   {
-      return _row.get<int>( "flat_length" );
+      // If the name is not in the fields, assume it's in
+      // the SOCI row.
+      if( it == _fields.end() )
+      {
+         switch( _row->get_properties( name ).get_data_type() )
+         {
+            case soci::dt_string:
+               return field_type( _row->get<std::string>( name ), STRING );
+
+            case soci::dt_double:
+               return field_type( _row->get<double>( name ), DOUBLE );
+
+            case soci::dt_integer:
+               return field_type( _row->get<int>( name ), INTEGER );
+
+            case soci::dt_unsigned_long_long:
+               return field_type( _row->get<unsigned long long>( name ), UNSIGNED_LONG_LONG );
+
+            case soci::dt_long_long:
+               return field_type( _row->get<long long>( name ), LONG_LONG );
+
+            default:
+               ASSERT( 0 );
+         }
+      }
+      else
+         return it->second;
    }
 
    std::ostream&
--- science_modules/base/src/galaxy.hh
+++ science_modules/base/src/galaxy.hh
@@ -1,22 +1,55 @@
 #ifndef tao_base_galaxy_hh
 #define tao_base_galaxy_hh
 
+#include <unordered_map>
+#include <boost/any.hpp>
+#include <boost/mpl/map.hpp>
+#include <boost/mpl/int.hpp>
+#include <boost/mpl/assert.hpp>
+#include <boost/mpl/at.hpp>
 #include <soci/soci.h>
 #include <libhpc/libhpc.hh>
 
 namespace tao {
+   using namespace hpc;
 
    class galaxy
    {
    public:
 
+      enum field_value_type
+      {
+         STRING = soci::dt_string,
+         DOUBLE = soci::dt_double,
+         INTEGER = soci::dt_integer,
+         UNSIGNED_LONG = soci::dt_unsigned_long,
+         LONG_LONG = soci::dt_long_long,
+         UNSIGNED_LONG_LONG = soci::dt_unsigned_long_long
+      };
+
       typedef double real_type;
+      typedef std::pair<boost::any,field_value_type> field_type;
+
+      typedef boost::mpl::map<mpl::pair<string, mpl::int_<STRING>>,
+                              mpl::pair<double, mpl::int_<DOUBLE>>,
+                              mpl::pair<int, mpl::int_<INTEGER>>,
+                              mpl::pair<unsigned long, mpl::int_<UNSIGNED_LONG> >,
+                              mpl::pair<long long, mpl::int_<LONG_LONG> >,
+                              mpl::pair<unsigned long long, mpl::int_<UNSIGNED_LONG_LONG>>> type_map;
 
    public:
 
       galaxy( const soci::row& row,
-              const hpc::array<real_type,3>& box,
-	      const hpc::string& table );
+	      const string& table );
+
+      galaxy();
+
+      void
+      update( const soci::row& row,
+              const string& table );
+
+      void
+      update( const galaxy& gal );
 
       void
       set_redshift( real_type redshift );
@@ -24,7 +57,7 @@ namespace tao {
       const soci::row&
       row() const;
 
-      const hpc::string&
+      const string&
       table() const;
 
       long long
@@ -54,14 +87,25 @@ namespace tao {
       real_type
       bulge_metallicity() const;
 
-      unsigned
-      flat_file() const;
+      template< class T >
+      void
+      set_field( const string& name,
+                 T value )
+      {
+         field_type& field = _fields[name];
+         field.first = value;
+         field.second = (field_value_type)boost::mpl::at<type_map,T>::type::value;
+      }
 
-      unsigned
-      flat_offset() const;
+      template< class T >
+      T
+      value( const string& name ) const
+      {
+         return boost::any_cast<T>( field( name ).first );
+      }
 
-      unsigned
-      flat_length() const;
+      field_type
+      field( const string& name ) const;
 
       friend std::ostream&
       operator<<( std::ostream& strm,
@@ -69,10 +113,10 @@ namespace tao {
 
    public:
 
-      const soci::row& _row;
-      const hpc::array<real_type,3>& _box;
-      const hpc::string& _table;
+      const soci::row* _row;
+      const string* _table;
       real_type _z;
+      std::unordered_map<string,field_type> _fields;
    };
 }
 
