5fb1378d50b1efb4c37323a28328314721166116
Amr Hassan <amr@amr-VirtualBox.(none)>
2013-03-05 16:39:15 +1100

    Latest version before adding multiple lightcone support

---
+++ core/workflow/LogReader.py
@@ -0,0 +1,65 @@
+import os, shlex, subprocess,string,time
+import EnumerationLookup
+import settingReader # Read the XML settings
+import logging
+
+
+
+class LogReader(object):
+      
+    
+    def __init__(self,Options):
+        self.Options=Options
+    
+    def GetFileName(self,UserName,JobID):
+        path = os.path.join(self.Options['WorkFlowSettings:WorkingDir'],'jobs', UserName, str(JobID),'log','tao.log')        
+        return path
+    def ParseFile(self,UserName,JobID):
+        FilePath=self.GetFileName(UserName, JobID)
+        JobDetails={'start':-1,'progress':'0%','end':-1,'error':'','endstate':''}
+        counter=10
+        while ((not os.path.exists(FilePath)) and counter>0):
+            counter=counter-1
+            logging.info('TAO LogFile not found... Will retry for more '+str(counter)+' times')
+            time.sleep(1)
+            
+        if(os.path.exists(FilePath)):
+            LogFile = open(FilePath, "r")
+            FileLines=LogFile.readlines()            
+        
+            for Line in FileLines:
+                JobDetails=self.ParseLine(Line,JobDetails)
+            
+            LogFile.close()
+            return JobDetails
+        else:
+            logging.error(FilePath+' Not Found')
+            return None
+    
+    def ParseLine(self,Line,JobDetails):  
+        Line=Line.lower().strip('\n')    
+        LineParts=Line.split(',')
+        if len(LineParts)>=2:
+            if LineParts[1]=='start':
+                JobDetails['start']=float(LineParts[0])
+            elif LineParts[1]=='end':
+                JobDetails['end']=float(LineParts[0])
+                JobDetails['endstate']=LineParts[2]                
+            elif LineParts[1]=='progress':
+                JobDetails['progress']=LineParts[2]
+                JobDetails['end']=float(LineParts[0])
+            elif LineParts[1]=='error':
+                JobDetails['error']=LineParts[2]    
+            
+        return JobDetails
+        
+        
+#if __name__ == '__main__':
+
+    ## Read Running Setting from XML File
+#    [Options]=settingReader.ParseParams("settings.xml")
+    
+#    LogReaderObj=LogReader(Options)
+#    LogReaderObj.ParseFile('UserName', 100)
+        
+              
---
+++ core/workflow/emailreport.py
@@ -0,0 +1,21 @@
+
+
+import smtplib
+from email.mime.text import MIMEText
+
+
+def SendEmailToAdmin(Options,Title,Contents):
+        
+    s = smtplib.SMTP(Options['WorkFlowSettings:emailserver'])
+    Recipients=[Options['WorkFlowSettings:ToEmail'],'alistair@intersect.org.au']
+    #Recipients=[Options['WorkFlowSettings:ToEmail']]
+    msg = MIMEText(Contents)
+    msg['Subject'] = Title
+    msg['From'] = Options['WorkFlowSettings:senderEmail']
+    msg['To'] = ','.join(Recipients)#Options['WorkFlowSettings:ToEmail']
+    
+    s.sendmail(Options['WorkFlowSettings:senderEmail'],Recipients,msg.as_string())
+    
+    
+    
+    s.quit()
