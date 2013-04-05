bd78357045c1686ea376423e299768954b7aeb24
Cindy <cindy@intersect.org.au>
2013-04-05 18:25:10 +1100

    ASVO-329 Add tests for display in the Summary & Submit tab

--- web/tao/tests/integration_tests/mock_galaxy_factory_tests.py
+++ web/tao/tests/integration_tests/mock_galaxy_factory_tests.py
@@ -1,10 +1,11 @@
+import HTMLParser
+
 from django.utils.html import strip_tags
 
-from tao.tests.integration_tests.helper import LiveServerTest, wait
-
-from tao.tests.support.factories import UserFactory, SimulationFactory, GalaxyModelFactory, DataSetFactory, JobFactory, DataSetPropertyFactory, DustModelFactory, StellarModelFactory, BandPassFilterFactory
 from tao.models import Simulation, StellarModel, DustModel, BandPassFilter
 from tao.settings import MODULE_INDICES
+from tao.tests.integration_tests.helper import LiveServerTest, wait
+from tao.tests.support.factories import UserFactory, SimulationFactory, GalaxyModelFactory, DataSetFactory, JobFactory, DataSetPropertyFactory, DustModelFactory, StellarModelFactory, BandPassFilterFactory
 
 class MockGalaxyFactoryTest(LiveServerTest):
 
@@ -36,7 +37,7 @@ class MockGalaxyFactoryTest(LiveServerTest):
         password = "funnyfish"
         self.user = UserFactory.create(username=username, password=password)
         self.login(username, password)
-        
+
         self.visit('mock_galaxy_factory')
 
     def tearDown(self):
@@ -46,18 +47,18 @@ class MockGalaxyFactoryTest(LiveServerTest):
         initial_selection = self.get_selected_option_text(self.lc_id('catalogue_geometry'))
         self.assertEqual('Light-Cone', initial_selection)
         self.assert_not_displayed(self.lc_id('box_size'))
-    
+
     def test_box_size_field_on_catalogue_geometry_change(self):
         self.select(self.lc_id('catalogue_geometry'), 'Box')
         self.assert_is_displayed(self.lc_id('box_size'))
-        
+
         self.select(self.lc_id('catalogue_geometry'), 'Light-Cone')
         self.assert_not_displayed(self.lc_id('box_size'))
-        
-    def test_sidebar_text_on_initial_load(self):    
+
+    def test_sidebar_text_on_initial_load(self):
         first_simulation = Simulation.objects.all()[0]
         first_galaxy_model = first_simulation.galaxymodel_set.all()[0]
-        
+
         self.assert_galaxy_model_info_shown(first_galaxy_model)
         self.assert_galaxy_model_options_correct_for_a_simulation(first_simulation)
         self.assert_simulation_info_shown(first_simulation)
@@ -66,22 +67,22 @@ class MockGalaxyFactoryTest(LiveServerTest):
         self.assert_sidebar_info_not_shown('stellar-model')
         self.assert_sidebar_info_not_shown('band-pass')
         self.assert_sidebar_info_not_shown('dust-model')
-    
-    def test_sidebar_text_on_simulation_change(self):      
+
+    def test_sidebar_text_on_simulation_change(self):
         second_simulation = Simulation.objects.all()[1]
-        
+
         self.select_dark_matter_simulation(second_simulation)
-        
+
         self.assert_galaxy_model_info_shown(second_simulation.galaxymodel_set.all()[0])
         self.assert_galaxy_model_options_correct_for_a_simulation(second_simulation)
         self.assert_simulation_info_shown(second_simulation)
-        
+
     def test_sidebar_text_on_galaxy_model_change(self):
         first_simulation = Simulation.objects.all()[0]
         second_galaxy_model = first_simulation.galaxymodel_set.all()[1]
-        
+
         self.select_galaxy_model(second_galaxy_model)
-        
+
         self.assert_galaxy_model_info_shown(second_galaxy_model)
 
     def test_sed_sidebar_text_on_apply_sed(self):
@@ -146,32 +147,25 @@ class MockGalaxyFactoryTest(LiveServerTest):
 
     def test_summary_on_initial_load(self):
         self.click('tao-tabs-' + LiveServerTest.SUMMARY_INDEX)
-        geometry_displayed = self.get_summary_field_text('light_cone', 'geometry_type')
         init_simulation = Simulation.objects.all()[0]
-        simulation_displayed = self.get_summary_field_text('light_cone', 'simulation')
         init_galaxy_model = init_simulation.galaxymodel_set.all()[0]
-        galaxy_model_displayed = self.get_summary_field_text('light_cone', 'galaxy_model')
-        output_properties_displayed = self.get_summary_field_text('light_cone', 'output_properties')
-        sed_displayed = self.get_summary_field_text('sed', 'select_sed')
-        filter_displayed = self.get_summary_field_text('record_filter', 'record_filter')
 
-        self.assertEqual('Light-Cone', geometry_displayed)
-        self.assertEqual(init_simulation.name, simulation_displayed)
-        self.assertEqual(init_galaxy_model.name, galaxy_model_displayed)
-        self.assertEqual('0 properties selected', output_properties_displayed)
-        self.assertEqual('Not selected', sed_displayed)
-        self.assertEqual('No Filter', filter_displayed)
-        self.assert_not_displayed('div.summary_light_cone .box_fields')
-        self.assert_not_displayed('div.summary_sed .apply_sed')
+        self.assert_summary_field_correctly_shown('Light-Cone', 'light_cone', 'geometry_type')
+        self.assert_summary_field_correctly_shown(init_simulation.name, 'light_cone', 'simulation')
+        self.assert_summary_field_correctly_shown(init_galaxy_model.name, 'light_cone', 'galaxy_model')
+        self.assert_summary_field_correctly_shown('0 properties selected', 'light_cone', 'output_properties')
+        self.assert_summary_field_correctly_shown('Not selected', 'sed', 'select_sed')
+        self.assert_summary_field_correctly_shown('No Filter', 'record_filter', 'record_filter')
+        self.assert_not_displayed(self.get_summary_selector('light_cone', 'box_fields'))
+        self.assert_not_displayed(self.get_summary_selector('sed', 'apply_sed'))
 
     def test_summary_on_geometry_change(self):
         self.select(self.lc_id('catalogue_geometry'), 'Box')
         self.click('tao-tabs-' + LiveServerTest.SUMMARY_INDEX)
-        geometry_displayed = self.get_summary_field_text('light_cone', 'geometry_type')
 
-        self.assertEqual('Box', geometry_displayed)
-        self.assert_is_displayed('div.summary_light_cone .box_fields')
-        self.assert_not_displayed('div.summary_light_cone .light_cone_fields')
+        self.assert_summary_field_correctly_shown('Box', 'light_cone', 'geometry_type')
+        self.assert_is_displayed(self.get_summary_selector('light_cone', 'box_fields'))
+        self.assert_not_displayed(self.get_summary_selector('light_cone', 'light_cone_fields'))
 
     def test_summary_on_simulation_change(self):
         second_simulation = Simulation.objects.all()[1]
@@ -186,43 +180,104 @@ class MockGalaxyFactoryTest(LiveServerTest):
         self.fill_in_fields({'max': max_input, 'min': min_input}, id_wrap=self.rf_id)
         self.click('tao-tabs-' + LiveServerTest.SUMMARY_INDEX)
         wait(1)
-        import HTMLParser
         h = HTMLParser.HTMLParser()
         if filter.units != '':
             expected_filter_display = min_input + h.unescape(' &le; ') + filter.label + ' (' + filter.units + ') '+ h.unescape(' &le; ') + max_input
         else:
             expected_filter_display = min_input + h.unescape(' &le; ') + filter.label + h.unescape(' &le; ') + max_input
-        simulation_displayed = self.get_summary_field_text('light_cone', 'simulation')
-        galaxy_model_displayed = self.get_summary_field_text('light_cone', 'galaxy_model')
-        filter_displayed = self.get_summary_field_text('record_filter', 'record_filter')
 
-        self.assertEqual(second_simulation.name, simulation_displayed)
-        self.assertEqual(init_galaxy_model_of_second_simulation.name, galaxy_model_displayed)
-        self.assertEqual(expected_filter_display, filter_displayed)
+        self.assert_summary_field_correctly_shown(second_simulation.name, 'light_cone', 'simulation')
+        self.assert_summary_field_correctly_shown(init_galaxy_model_of_second_simulation.name, 'light_cone', 'galaxy_model')
+        self.assert_summary_field_correctly_shown(expected_filter_display, 'record_filter', 'record_filter')
 
     def test_summary_on_dataset_description_expand(self):
         # in dataset, stellar model, and dust model, clicking on ">>" expands to show the description
         # ">>" rotates to show the current state: expanded or not
         init_simulation = Simulation.objects.all()[0];
         init_galaxy_model = init_simulation.galaxymodel_set.all()[0]
+
         self.click('tao-tabs-' + LiveServerTest.SUMMARY_INDEX)
+        self.assert_not_displayed(self.get_summary_selector('light_cone', 'simulation_description'))
+        self.assert_not_displayed(self.get_summary_selector('light_cone', 'galaxy_model_description'))
         self.assertEqual('>>', self.selenium.find_element_by_id('expand_dataset').text)
         self.click('expand_dataset')
+        self.assertEqual('<<', self.selenium.find_element_by_id('expand_dataset').text)
         expected_simulation_details = init_simulation.name + ':\n' + strip_tags(init_simulation.details)
         expected_galaxy_model_details = init_galaxy_model.name + ':\n' + strip_tags(init_galaxy_model.details)
-        simulation_details_displayed = strip_tags(self.get_summary_field_text('light_cone', 'simulation_description'))
-        galaxy_model_details_displayed = strip_tags(self.get_summary_field_text('light_cone', 'galaxy_model_description'))
-        self.assertEqual(expected_simulation_details, simulation_details_displayed)
-        self.assertEqual(expected_galaxy_model_details, galaxy_model_details_displayed)
-        self.assertEqual('<<', self.selenium.find_element_by_id('expand_dataset').text)
+        self.assert_summary_field_correctly_shown(expected_simulation_details, 'light_cone', 'simulation_description')
+        self.assert_summary_field_correctly_shown(expected_galaxy_model_details, 'light_cone', 'galaxy_model_description')
+        self.assert_is_displayed(self.get_summary_selector('light_cone', 'simulation_description'))
+        self.assert_is_displayed(self.get_summary_selector('light_cone', 'galaxy_model_description'))
+
+        self.click('expand_dataset')
+        self.assertEqual('>>', self.selenium.find_element_by_id('expand_dataset').text)
+        self.assert_not_displayed(self.get_summary_selector('light_cone', 'simulation_description'))
+        self.assert_not_displayed(self.get_summary_selector('light_cone', 'galaxy_model_description'))
+
+    def test_summary_on_filter_list_expand(self):
+        # Clicking on ">>" in output properties or bandpass filters expands the div and lists the properties in an unordered list
+        self.click('tao-tabs-' + LiveServerTest.SUMMARY_INDEX)
+        self.assert_not_displayed(self.get_summary_selector('light_cone', 'output_properties_list'))
+
+        self.click('tao-tabs-' + MODULE_INDICES['light_cone'])
+        self.click(self.lc_2select('op_add_all'))
+        self.click('tao-tabs-' + LiveServerTest.SUMMARY_INDEX)
+        self.click('expand_output_properties')
+
 
     def test_summary_on_light_cone_dimensions_change(self):
-        #range displays should be intelligent, i.e. if the min or max is blank, it isn't displayed
-        pass
+        self.click('tao-tabs-' + LiveServerTest.SUMMARY_INDEX)
+        self.assert_summary_field_correctly_shown('', 'light_cone', 'ra_opening_angle')
+        self.assert_summary_field_correctly_shown('', 'light_cone', 'dec_opening_angle')
+        self.assert_summary_field_correctly_shown('', 'light_cone', 'redshift_min')
+        self.assert_summary_field_correctly_shown('', 'light_cone', 'redshift_max')
 
-    def test_summary_on_filter_expand(self):
-        # Clicking on ">>" in output properties or bandpass filters expands the div and lists the properties in an unordered list
-        pass
+        ra_opening_angle = '1'
+        dec_opening_angle = '2'
+        redshift_min = '3'
+        redshift_max = '4'
+        h = HTMLParser.HTMLParser()
+        self.click('tao-tabs-' + MODULE_INDICES['light_cone'])
+        self.fill_in_fields({'ra_opening_angle': ra_opening_angle, 'dec_opening_angle': dec_opening_angle, 'redshift_min': redshift_min, 'redshift_max': redshift_max}, id_wrap=self.lc_id)
+        self.click('tao-tabs-' + LiveServerTest.SUMMARY_INDEX)
+        self.assert_summary_field_correctly_shown('RA: '+ra_opening_angle+h.unescape('&deg;')+',', 'light_cone', 'ra_opening_angle')
+        self.assert_summary_field_correctly_shown('Dec: '+dec_opening_angle+h.unescape('&deg;'), 'light_cone', 'dec_opening_angle')
+        self.assert_summary_range_correctly_shown('Redshift: ' + redshift_min + h.unescape(' &le;') + ' r ' + h.unescape('&le;') + redshift_max, 'light_cone', ['redshift_min', 'redshift_max'])
+
+        #range displays should be intelligent, i.e. if the min or max is blank, it isn't displayed
+        self.click('tao-tabs-' + MODULE_INDICES['light_cone'])
+        self.clear(self.lc_id('redshift_min'))
+        self.click('tao-tabs-' + LiveServerTest.SUMMARY_INDEX)
+        self.assert_summary_range_correctly_shown('Redshift: r ' + h.unescape('&le;') + ' 4', 'light_cone', ['redshift_min', 'redshift_max'])
+        self.click('tao-tabs-' + MODULE_INDICES['light_cone'])
+        self.clear(self.lc_id('redshift_max'))
+        self.click('tao-tabs-' + LiveServerTest.SUMMARY_INDEX)
+        self.assert_summary_range_correctly_shown('', 'light_cone', ['redshift_min', 'redshift_max'])
+        self.click('tao-tabs-' + MODULE_INDICES['light_cone'])
+        self.fill_in_fields({'redshift_min': 5}, id_wrap=self.lc_id)
+        self.click('tao-tabs-' + LiveServerTest.SUMMARY_INDEX)
+        self.assert_summary_range_correctly_shown('Redshift: 5 ' + h.unescape('&le;') + ' r', 'light_cone', ['redshift_min', 'redshift_max'])
+
+    def test_summary_on_light_cone_count_and_type(self):
+        ra_opening_angle = '1'
+        dec_opening_angle = '2'
+        redshift_min = '3'
+        redshift_max = '4'
+        number_of_light_cones = '5'
+        self.fill_in_fields({'ra_opening_angle': ra_opening_angle, 'dec_opening_angle': dec_opening_angle, 'redshift_min': redshift_min, 'redshift_max': redshift_max}, id_wrap=self.lc_id)
+        self.click('tao-tabs-' + LiveServerTest.SUMMARY_INDEX)
+        self.assert_summary_field_correctly_shown('1 unique light cones', 'light_cone', 'number_of_light_cones')
+
+        self.click('tao-tabs-' + MODULE_INDICES['light_cone'])
+        self.clear(self.lc_id('number_of_light_cones'))
+        self.fill_in_fields({'number_of_light_cones': number_of_light_cones}, id_wrap=self.lc_id)
+        self.click('tao-tabs-' + LiveServerTest.SUMMARY_INDEX)
+        self.assert_summary_field_correctly_shown(number_of_light_cones + ' unique light cones', 'light_cone', 'number_of_light_cones')
+
+        self.click('tao-tabs-' + MODULE_INDICES['light_cone'])
+        self.click_by_css(self.lc_id('light_cone_type_1')) # select "random"
+        self.click('tao-tabs-' + LiveServerTest.SUMMARY_INDEX)
+        self.assert_summary_field_correctly_shown(number_of_light_cones + ' random light cones', 'light_cone', 'number_of_light_cones')
 
     def get_field_by_value_in_control_group(self, value, name):
         control_group = self.selenium.find_elements_by_name(name)
@@ -289,6 +344,17 @@ class MockGalaxyFactoryTest(LiveServerTest):
         self.click(self.sed('apply_dust'))
         self.assert_is_unchecked(self.sed_id('apply_dust'))
         self.assert_is_disabled(self.sed_id('select_dust_model'))
+
+    def assert_summary_field_correctly_shown(self, expected_value, form_name, field_name):
+        value_displayed = self.get_summary_field_text(form_name, field_name)
+        self.assertEqual(expected_value, strip_tags(value_displayed))
+        # self.assert_is_displayed(self.get_summary_selector(form_name, field_name))
+
+    def assert_summary_range_correctly_shown(self, expected_value, form_name, field_names):
+        value_displayed = ''
+        for field_name in field_names:
+            value_displayed += self.get_summary_field_text(form_name, field_name)
+        self.assertEqual(expected_value, value_displayed)
 
     def assert_simulation_info_shown(self, simulation):
         """  check the name of the simulation in the sidebar is the same as simulation_name
