21b942c1e2991c29084c337b1e0490e86aa8c801
Alistair Grant <akgrant@gmail.com>
2013-06-11 14:20:55 +0200

    Settings: Jobs are now submitted instead of held

--- web/tao/settings.py
+++ web/tao/settings.py
@@ -217,7 +217,7 @@ API_ALLOWED_IPS = (
                    '127.0.0.1',
                    )
 
-INITIAL_JOB_STATUS = 'HELD'
+INITIAL_JOB_STATUS = 'SUBMITTED'
 
 #
 # To avoid changing the directory structure until after we have confirmed
