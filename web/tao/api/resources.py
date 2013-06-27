f77594f19fdfeec60923eafb7a6f16b9860ba9e1
Cindy <cindy@intersect.org.au>
2013-06-27 18:28:40 +1000

    ASVO-403 Give authorization to put command to enable updating job workflow status

--- web/tao/api/resources.py
+++ web/tao/api/resources.py
@@ -1,11 +1,17 @@
+from tastypie import fields
 from tastypie.resources import ModelResource
+from tastypie.authorization import Authorization
 from tao.models import WorkflowCommand
 
+
 class WorkflowCommandResource(ModelResource):
+
     class Meta:
         queryset = WorkflowCommand.objects.all()
         fields = ['id', 'jobid', 'submittedby', 'command', 'parameters', 'execution_status', 'execution_comment']
         allowed_methods = ['get', 'put']
+        authorization = Authorization()
         filtering = {
             "execution_status": 'exact',
+            "jobid": 'exact',
         }
--- web/wfcommand-update.py
+++ web/wfcommand-update.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
 Call the workflow command api to demonstrate simulating the backend.
 
@@ -20,36 +21,50 @@ It also doesn't do any error checking, etc.  The idea is to read the code.
 
 import sys
 import requests
+import json
 
 base_url = "http://127.0.0.1:8000/api/v1/workflowcommand/"
 
 if len(sys.argv) < 4:
-    print("usage: {0} job_id status comment".format(sys.argv[0]))
+    print("usage: {0} wf_id status comment".format(sys.argv[0]))
     exit(0)
 
-job_id = sys.argv[1]
-status = sys.argv[2]
+wf_id = sys.argv[1]
+status = sys.argv[2].upper()
 comment = sys.argv[3]
 
 data = {
-        'id': job_id,
+        'id': wf_id,
         'execution_status': status,
         'execution_comment': comment,
 }
-print("Job ID: {0}".format(job_id))
+print("Job ID: {0}".format(wf_id))
 print("Status: {0}".format(status))
-print("Comment: {0}".format(comment))
+print("Comment: {0}\n\n".format(comment))
 
 print("URL: {0}".format(base_url))
 response = requests.get(base_url)
 print("Response: {0}".format(response))
+print(json.dumps(json.loads(response.content),
+    sort_keys=True, indent=4, separators=(',',':')))
 
-url = base_url + job_id
+url = base_url + wf_id
 print("URL: {0}".format(url))
 response = requests.put(url, data)
 print("Response: {0}".format(response))
+print(json.dumps(json.loads(response.content),
+    sort_keys=True, indent=4, separators=(',',':')))
 
 url = base_url + '?execution_status=' + status
 print("URL: {0}".format(url))
 response = requests.get(url)
 print("Response: {0}".format(response))
+print(json.dumps(json.loads(response.content),
+    sort_keys=True, indent=4, separators=(',',':')))
+
+
+print("URL: {0}".format(url))
+response = requests.put(url, data)
+print("Response: {0}".format(response))
+print(json.dumps(json.loads(response.content),
+    sort_keys=True, indent=4, separators=(',',':')))
