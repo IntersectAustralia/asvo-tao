213e69a785a9807c80c21ed50c67782ab0affcf2
amraziz8 <ahassan@swin.edu.au>
2013-07-09 16:44:36 +1000

    Support run profiling code

--- core/workflow/ParseProfileData.py
+++ core/workflow/ParseProfileData.py
@@ -4,13 +4,14 @@ import settingReader # Read the XML settings
 import StringIO
 import psycopg2
 from psycopg2 import extras
-
+import os, shlex, subprocess,string
 
 class ParseProfileData(object):
 
     
 
     def __init__(self,FileName,Options):        
+        self.RunProfiling()
         self.Options=Options
         self.ProfileFileName=FileName
         self.LoadDBMetaData("millennium_full_hdf5_dist")
@@ -49,7 +50,14 @@ class ParseProfileData(object):
             return TablesList
         else:
             return []
-        
+    
+    
+    def RunProfiling(self):
+        #stdout = subprocess.check_output(shlex.split('ssh g2 \"cd %s; qsub -q %s %s\"'%(path.encode(locale.getpreferredencoding()), queuename.encode(locale.getpreferredencoding()), ScriptFileName.encode(locale.getpreferredencoding()))))
+        os.chdir('/home/amr/workspace/AppRun')
+        stdout = subprocess.check_output(shlex.split('/home/amr/workspace/asvo-tao-science/science_modules/build-optimised/bin/tao /home/amr/workspace/AppRun/params0.xml /home/amr/workspace/AppRun/basicsetting.xml'))
+        print stdout
+            
     def ParseFile(self):
         f=open(self.ProfileFileName,'r')
         Lines=f.readlines()
@@ -71,6 +79,7 @@ class ParseProfileData(object):
 
                 
 if __name__ == '__main__':
+    
      [Options]=settingReader.ParseParams("settings.xml")
      ParseProfileDataObj=ParseProfileData('/home/amr/workspace/AppRun/params0_tao.Profile.log00000',Options)
      [Boxes,Tables,Galaxies,Trees]=ParseProfileDataObj.ParseFile()
