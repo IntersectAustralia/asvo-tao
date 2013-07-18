3885726edfdc25337d7428592915f9da7f95b15e
Amr Hassan <ahassan@swin.edu.au>
2013-07-18 10:46:28 +1000

    Fix the ASVO-489 for votable and fits

--- science_modules/modules/src/votable.hh
+++ science_modules/modules/src/votable.hh
@@ -42,13 +42,13 @@ namespace tao {
 
       void open();
 
-      void process_galaxy( const tao::galaxy& galaxy );
+      void process_galaxy( tao::galaxy& galaxy );
 
       virtual void log_metrics();
 
    protected:
       string _xml_encode(string _toencode_string);
-      void _write_field( const tao::galaxy& galaxy,const string& field,unsigned idx);
+      void _write_field( const tao::galaxy& galaxy,const string& field);
       void _write_file_header(const string& ResourceName,const string& TableName );
       void _write_footer();
 
