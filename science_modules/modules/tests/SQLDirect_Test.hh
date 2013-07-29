bac717771220d69098bc6c3edb98307657ba6121
amraziz8 <ahassan@swin.edu.au>
2013-07-30 09:29:43 +1000

    Add SQLDirect Test Class

---
+++ science_modules/modules/tests/SQLDirect_Test.hh
@@ -0,0 +1,78 @@
+#include <soci/soci.h>
+#include <soci/sqlite3/soci-sqlite3.h>
+#include <cxxtest/TestSuite.h>
+#include <libhpc/libhpc.hh>
+#include <vector>
+#include <iostream>
+#include <pugixml.hpp>
+
+#include "tao/base/base.hh"
+#include "tao/modules/modules.hh"
+#include "tao/modules/sqldirect.hh"
+
+using namespace std;
+using namespace hpc;
+using namespace tao;
+#define NDEBUG
+
+
+
+
+class SQLDirect_suite : public CxxTest::TestSuite
+{
+public:
+
+
+	void test_SQLDirect()
+	{
+
+
+		LOG_PUSH( new logging::file( "TestLog.log" ) );
+		TS_TRACE("Starting SQLDirect Test");
+		string ParamXMLFile="params0.xml";
+		string BasicXMLFile="basicsetting.xml";
+		options::xml_dict xml;
+
+		xml.read( ParamXMLFile, "/tao" );
+		xml.read( BasicXMLFile );
+
+		TS_TRACE("XML Read Done");
+
+		sqldirect sqlobject;
+		sqlobject.initialise(xml);
+		/*multidb db("bolshoi_full_dist","tree_");
+		db.AddNewServer("tao01.hpc.swin.edu.au","taoadmin","password","3306");
+		db.AddNewServer("tao02.hpc.swin.edu.au","taoadmin","password","3306");
+
+
+		db.OpenAllConnections();
+		db.RestartAllConnections();
+		db.CloseAllConnections();
+
+		db["tree_1"];
+
+		TS_TRACE("Tree Loading Test");
+		soci::session* OldConnection=NULL;
+		for(int j=0;j<10;j++)
+		{
+			soci::session*  CurrentConnection=db.GetConnectionToAnyServer();
+			if(CurrentConnection!=OldConnection)
+			{
+				TS_TRACE("New Connection");
+				OldConnection=CurrentConnection;
+			}
+			else
+				ASSERT( 0 );
+		}
+		TS_TRACE("Start SQL");
+		db.ExecuteNoQuery_AllServers("Create Table Test(Id int);");
+		db.ExecuteNoQuery_AllServers("Drop Table Test;");
+		*/
+
+
+		TS_TRACE("End Setting Test");
+
+
+	}
+
+};
