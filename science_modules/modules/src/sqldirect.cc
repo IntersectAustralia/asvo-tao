cb56f5f591756c572cf36f1e909dffe36a8a7bb1
amraziz8 <ahassan@swin.edu.au>
2013-08-06 14:52:43 +1000

    SQL iterator

--- science_modules/modules/src/sqldirect.cc
+++ science_modules/modules/src/sqldirect.cc
@@ -116,10 +116,15 @@ namespace tao {
 		soci::rowset<soci::row> rs= (*_db)[(*_Tables_it)].prepare << query;
 		int rowscount=0;
 
+		_gal.clear();
+		_gal.set_table( *_Tables_it );
+
 		if(IsFirstCall==true)
 		{
 			soci::row const& firstrow = *(rs.begin());
-
+			_field_stor.reallocate( firstrow.size() );
+			_field_types.reallocate( firstrow.size() );
+			_field_names.reallocate( firstrow.size() );
 			for(std::size_t i = 0; i != firstrow.size(); ++i)
 			{
 				const soci::column_properties & props = firstrow.get_properties(i);
@@ -128,44 +133,117 @@ namespace tao {
 
 				switch(props.get_data_type())
 				{
-				case soci::dt_string:
-					LOGDLN( "Field Name: ", props.get_name(), " String" );
-					break;
-				case soci::dt_double:
-					LOGDLN( "Field Name: ", props.get_name(), " Double" );
-					break;
-				case soci::dt_integer:
-					LOGDLN( "Field Name: ", props.get_name(), " Integer" );
-					break;
-				case soci::dt_unsigned_long:
-					LOGDLN( "Field Name: ", props.get_name(), " Long" );
-					break;
-				case soci::dt_long_long:
-					LOGDLN( "Field Name: ", props.get_name(), " Long Long" );
-					break;
-				case soci::dt_date:
-					LOGDLN( "Field Name: ", props.get_name(), " Date" );
-
-					break;
+					case soci::dt_string:
+						LOGDLN( "Field Name: ", props.get_name(), " String" );
+						_field_types[i] = galaxy::STRING;
+						_field_stor[i] = new vector<string>( _batch_size );
+						_field_names[i]=props.get_name();
+						break;
+					case soci::dt_double:
+						LOGDLN( "Field Name: ", props.get_name(), " Double" );
+						_field_types[i] = galaxy::DOUBLE;
+						_field_stor[i] = new vector<double>( _batch_size );
+						_field_names[i]=props.get_name();
+						break;
+					case soci::dt_integer:
+						LOGDLN( "Field Name: ", props.get_name(), " Integer" );
+						_field_types[i] = galaxy::INTEGER;
+						_field_stor[i] = new vector<int>( _batch_size );
+						_field_names[i]=props.get_name();
+						break;
+					case soci::dt_unsigned_long_long:
+						LOGDLN( "Field Name: ", props.get_name(), " unsigned Long Long" );
+						_field_types[i] = galaxy::UNSIGNED_LONG_LONG;
+						_field_stor[i] = new vector<unsigned long long>( _batch_size );
+						_field_names[i]=props.get_name();
+						break;
+					case soci::dt_long_long:
+						LOGDLN( "Field Name: ", props.get_name(), " Long Long" );
+						_field_types[i] = galaxy::LONG_LONG;
+						_field_stor[i] = new vector<long long>( _batch_size );
+						_field_names[i]=props.get_name();
+						break;
+					default:
+						ASSERT( 0 );
 				}
-
-
 			}
 		}
-
-
-
-
-
-
 
 
 
 		for (soci::rowset<soci::row>::const_iterator it = rs.begin(); it != rs.end(); ++it)
 		{
 			soci::row const& currentrow = *it;
-			//LOGDLN( "New Row: ", rowscount++ );
+
+
+			for(std::size_t i = 0; i != currentrow.size(); ++i)
+			{
+				const soci::column_properties & props = currentrow.get_properties(i);
+
+				switch(props.get_data_type())
+				{
+					case soci::dt_string:
+						((vector<string>*)_field_stor[i])->push_back(currentrow.get<string>(i));
+						break;
+					case soci::dt_double:
+						((vector<double>*)_field_stor[i])->push_back(currentrow.get<double>(i));
+						break;
+					case soci::dt_integer:
+						((vector<int>*)_field_stor[i])->push_back(currentrow.get<int>(i));
+						break;
+					case soci::dt_unsigned_long_long:
+						((vector<unsigned long long>*)_field_stor[i])->push_back(currentrow.get<unsigned long long>(i));
+						break;
+					case soci::dt_long_long:
+						((vector<long long>*)_field_stor[i])->push_back(currentrow.get<long long>(i));
+						break;
+					default:
+						ASSERT( 0 );
+				}
+
+			}
+			rowscount++;
+			if (rowscount%10000==0)
+				LOGDLN( "New Row: ", rowscount);
 		}
+
+		for(int i = 0; i < _field_types.size(); i++)
+		{
+			switch( _field_types[i] )
+			{
+				case galaxy::STRING:
+					LOGDLN( "Set Field Name: ", _field_names[i], " To Galaxy" );
+					_gal.set_batch_size( ((vector<string>*)_field_stor[i])->size() );
+					_gal.set_field<string>( _field_names[i], *(vector<string>*)_field_stor[i] );
+					break;
+				case galaxy::DOUBLE:
+					LOGDLN( "Set Field Name: ", _field_names[i], " To Galaxy" );
+					_gal.set_batch_size( ((vector<double>*)_field_stor[i])->size() );
+					_gal.set_field<double>( _field_names[i], *(vector<double>*)_field_stor[i] );
+					break;
+				case galaxy::INTEGER:
+					LOGDLN( "Set Field Name: ", _field_names[i], " To Galaxy" );
+					_gal.set_batch_size( ((vector<int>*)_field_stor[i])->size() );
+					_gal.set_field<int>( _field_names[i], *(vector<int>*)_field_stor[i] );
+					break;
+				case galaxy::UNSIGNED_LONG_LONG:
+					LOGDLN( "Set Field Name: ", _field_names[i], " To Galaxy" );
+					_gal.set_batch_size( ((vector<unsigned long long>*)_field_stor[i])->size() );
+					_gal.set_field<unsigned long long>( _field_names[i], *(vector<unsigned long long>*)_field_stor[i] );
+					break;
+				case galaxy::LONG_LONG:
+					LOGDLN( "Set Field Name: ", _field_names[i], " To Galaxy" );
+					_gal.set_batch_size( ((vector<long long>*)_field_stor[i])->size() );
+					_gal.set_field<long long>( _field_names[i], *(vector<long long>*)_field_stor[i] );
+					break;
+				default:
+					ASSERT( 0 );
+			}
+		}
+
+
+
+
 
 	}
 
