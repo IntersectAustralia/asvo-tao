2b96bfe093d45a7b1ac56f7cbddd4314795c31ab
Alistair Grant <akgrant@gmail.com>
2013-08-14 20:05:39 +1000

    ASVO-511: Mock Image in Job View

--- ui/mock_image/taoui_mock_image/forms.py
+++ ui/mock_image/taoui_mock_image/forms.py
@@ -156,13 +156,14 @@ class Form(BaseForm):
             data = args[1]
             data = data.copy()
 
-            # Get the management total forms count and add back in
-            # the 1 we subtracted.
-            total = data.get('mock_image-TOTAL_FORMS', 0)
-            if total:
-                total = int(total) + 1
-            else:
-                total = 1
+            # Removed the + 1 since it doesn't appear to have been subtracted
+            ## Get the management total forms count and add back in
+            ## the 1 we subtracted.
+            total = data.get('mock_image-TOTAL_FORMS', 1)
+#             if total:
+#                 total = int(total) + 1
+#             else:
+#                 total = 1
 
             # Do a final check for the checkbox, if we are disabled
             # then set the count to 1.
