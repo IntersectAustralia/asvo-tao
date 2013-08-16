4d19d1877619e271bc2072797604d9e6b8025f31
Carlos Aya <carlos@intersect.org.au>
2013-08-16 15:58:08 +1000

    Bug: Ensure job summry is ok after changes to back-end form behaviour

--- web/tao/utils.py
+++ web/tao/utils.py
@@ -10,4 +10,4 @@ from tao.models import GlobalParameter
 def output_formats():
     """Answer the list of supported output formats."""
     of_string = GlobalParameter.objects.get(parameter_name="output_formats").parameter_value
-    return eval(of_string)
+    return eval(of_string.replace('\r',''))
