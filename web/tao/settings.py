b339d3626673a55115a6512d01d81ac287be3cbc
Carlos Aya <carlos@intersect.org.au>
2013-08-19 13:02:24 +1000

    Version 0.26.4

--- web/tao/settings.py
+++ web/tao/settings.py
@@ -262,7 +262,7 @@ MODULE_INDICES = {
                   'telescope': '8'
                   }
 
-TAO_VERSION = '0.26.3'
+TAO_VERSION = '0.26.4'
 
 AAF_DS_URL = 'https://ds.test.aaf.edu.au/discovery/DS'
 AAF_APP_ID = 'https://example.intersect.org.au/shibboleth'
