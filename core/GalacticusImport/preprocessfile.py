c167f801cb7adb24cbb7c804d317bdfee61b5ef7
Amr Hassan <ahassan@swin.edu.au>
2013-08-22 19:40:29 +1000

    Execule the non-common fields

--- core/GalacticusImport/preprocessfile.py
+++ core/GalacticusImport/preprocessfile.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     OutputFile=h5py.File(OutputFilePath,'w')
     
 
-    cosgroup=OutputFile.create_group("cosmology")
+    '''cosgroup=OutputFile.create_group("cosmology")
     cosgroup.create_dataset("hubble",  data=[0.0])
     cosgroup.create_dataset("omega_l",  data=[0.0])
     cosgroup.create_dataset("omega_m",  data=[0.0])
@@ -33,82 +33,94 @@ if __name__ == '__main__':
     
    
     
-    #Datadict={}
-    TotalCount=0
-    dtypestr=""
-    fieldNames=""
+    
+   
+    
     DataTypeLists=[]
-    for dset in Outputs["Output1/nodeData"]:    
-        DataTypeLists+=[(str(dset),(str(Outputs["Output1/nodeData/"+str(dset)].dtype)))]
+    for dset in Outputs["Output15/nodeData"]:
+        print "FieldName: "+str(dset)    
+        DataTypeLists+=[(str(dset),(str(Outputs["Output15/nodeData/"+str(dset)].dtype)))]
     
-    DataTypeLists+=[('zoneid',numpy.int32)]
-    
-    
+    DataTypeLists+=[('zoneid',numpy.int32)]    
     DataTypeLists+=[('MetalsColdGas',numpy.float64)]
     DataTypeLists+=[('sfr',numpy.float64)]  
     DataTypeLists+=[('treeid',numpy.int64)]
     DataTypeLists+=[('galaxyglobalindex',numpy.int64)]  
     
-    
-    
-    
+  
     
     
     TotalRecordsCount=0
     for Output in Outputs:
-        FirstKey=Outputs[Output+"/nodeData"].keys()[0]        
-        TotalRecordsCount=TotalRecordsCount+ len(Outputs[Output+"/nodeData/"+FirstKey][:])
+        if len(Outputs[Output+"/nodeData"].keys())>0:
+            FirstKey=Outputs[Output+"/nodeData"].keys()[0]        
+            TotalRecordsCount=TotalRecordsCount+ len(Outputs[Output+"/nodeData/"+FirstKey][:])
     print "Total:"+str(TotalRecordsCount)
     Datadict = numpy.zeros(shape=(TotalRecordsCount,),dtype=DataTypeLists)
+    
     
 
     EndIndex=0
     OutputCounter=0
     GalaxyGlobalIndex=0
     for Output in Outputs:
-        print Output+"\t"+str(OutputCounter)+"/"+str(len(Outputs))
-        OutputCounter=OutputCounter+1
-        TreeIDsList=[]
-        GalaxyGlobalIndexList=[]
-        for LocalIndex in range(0,len(Outputs[Output+'/mergerTreeStartIndex'])):
-                       
-            if Outputs[Output+'/mergerTreeCount'][LocalIndex]>0:               
-                #TreeStartIndexData.append(NextItemIndex)
-                #GalaxyCountData.append(Outputs[Output+'/mergerTreeCount'][LocalIndex])
-                TreeIndexData.append(Outputs[Output+'/mergerTreeIndex'][LocalIndex])  
-                for i in range(0,Outputs[Output+'/mergerTreeCount'][LocalIndex]):
-                    TreeIDsList.append(Outputs[Output+'/mergerTreeIndex'][LocalIndex])
-                    GalaxyGlobalIndexList.append(GalaxyGlobalIndex)  
-                    GalaxyGlobalIndex=GalaxyGlobalIndex+1                        
-                #NextItemIndex=NextItemIndex+Outputs[Output+'/mergerTreeCount'][LocalIndex]
+        
+        if len(Outputs[Output+"/nodeData"].keys())>0:
             
-            TotalCount=TotalCount+ Outputs[Output+'/mergerTreeCount'][LocalIndex]    
-        StartIndex=EndIndex        
-        FirstKey=Outputs[Output+"/nodeData"].keys()[0] 
-        EndIndex=StartIndex+len(Outputs[Output+"/nodeData/"+FirstKey][:])
-        print str(StartIndex)+":"+ str(EndIndex)
-        print len(Outputs[Output+"/nodeData/"+str(dset)][:])
-        for dset in Outputs[Output+"/nodeData"]:                                
-            Datadict[dset][StartIndex:EndIndex]=Outputs[Output+"/nodeData/"+str(dset)][:]
-        ZoneIndex=int(Output.replace("Output","")) 
-        print "ZoneIndex="+str(ZoneIndex)
-        Datadict['zoneid'][StartIndex:EndIndex]=ZoneIndex
-        Datadict['treeid'][StartIndex:EndIndex]=TreeIDsList 
-        Datadict['galaxyglobalindex'][StartIndex:EndIndex]= GalaxyGlobalIndexList  
+            print Output+"\t"+str(OutputCounter)+"/"+str(len(Outputs))
+            OutputCounter=OutputCounter+1
+            TreeIDsList=[]
+            GalaxyGlobalIndexList=[]
+            for LocalIndex in range(0,len(Outputs[Output+'/mergerTreeStartIndex'])):
+                           
+                if Outputs[Output+'/mergerTreeCount'][LocalIndex]>0:               
+                    #TreeStartIndexData.append(NextItemIndex)
+                    #GalaxyCountData.append(Outputs[Output+'/mergerTreeCount'][LocalIndex])
+                    TreeIndexData.append(Outputs[Output+'/mergerTreeIndex'][LocalIndex])  
+                    for i in range(0,Outputs[Output+'/mergerTreeCount'][LocalIndex]):
+                        TreeIDsList.append(Outputs[Output+'/mergerTreeIndex'][LocalIndex])
+                        GalaxyGlobalIndexList.append(GalaxyGlobalIndex)  
+                        GalaxyGlobalIndex=GalaxyGlobalIndex+1                        
+                    #NextItemIndex=NextItemIndex+Outputs[Output+'/mergerTreeCount'][LocalIndex]
                 
-        StartIndex=EndIndex+1
+                    
+            StartIndex=EndIndex        
+            FirstKey=Outputs[Output+"/nodeData"].keys()[0] 
+            EndIndex=StartIndex+len(Outputs[Output+"/nodeData/"+FirstKey][:])
+            print str(StartIndex)+":"+ str(EndIndex)
+            #print len(Outputs[Output+"/nodeData/"+str(dset)][:])
+            for dset in Outputs[Output+"/nodeData"]:
+                if dset in Datadict.dtype.fields:                                
+                    Datadict[dset][StartIndex:EndIndex]=Outputs[Output+"/nodeData/"+str(dset)][:]
+                else:
+                    print "Field ("+dset+") does not exists"
+            
+            ZoneIndex=int(Output.replace("Output","")) 
+            print "ZoneIndex="+str(ZoneIndex)
+            Datadict['zoneid'][StartIndex:EndIndex]=ZoneIndex
+            Datadict['treeid'][StartIndex:EndIndex]=TreeIDsList 
+            Datadict['galaxyglobalindex'][StartIndex:EndIndex]= GalaxyGlobalIndexList  
+                    
+            StartIndex=EndIndex+1
+        else:
+            print "Skipping: "+Output
     
     Datadict['MetalsColdGas']=numpy.add(Datadict['diskAbundancesGasMetals'],Datadict['spheroidAbundancesGasMetals'])
     Datadict['sfr']=numpy.add(Datadict['interOutputDiskStarFormationRate'],Datadict['interOutputSpheroidStarFormationRate'])
     
     numpy.save('numpyDataunsorted.npy', Datadict)     
-    numpy.sort(Datadict, order='treeid')
-    numpy.save('numpyData.npy', Datadict)
-
+    numpy.sort(Datadict, order='mergerTreeIndex')
+    numpy.save('numpyData.npy', Datadict)'''
+    Datadict=numpy.load('numpyData.npy')
     OutputFile.create_dataset("galaxies", data=Datadict)
-    
-    for row in Datadict:
-        print row
+    ListOfUnique=numpy.unique(Datadict['mergerTreeIndex'])
+    ArrayList=Datadict['mergerTreeIndex']
+        
+        
+    for TreeId in  ListOfUnique:
+        itemindex=numpy.where(Datadict['mergerTreeIndex']==TreeId)
+        print itemindex
+        
     
     OutputFile.create_dataset("tree_counts", data=GalaxyCountData)
     OutputFile.create_dataset("tree_displs", data=TreeStartIndexData)
