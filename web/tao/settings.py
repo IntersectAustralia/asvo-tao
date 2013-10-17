2d7dfd70691a9ca94deed5d5c2adec25bb10e1e6
Ilya Anisimoff <ilya@intersect.org.au>
2013-10-18 09:57:31 +1100

    MISC: Updates version to 1.0.7

--- web/tao/settings.py
+++ web/tao/settings.py
@@ -261,7 +261,7 @@ MODULE_INDICES = {
                   'telescope': '8'
                   }
 
-TAO_VERSION = '1.0.6'
+TAO_VERSION = '1.0.7'
 
 AAF_DS_URL = 'https://ds.test.aaf.edu.au/discovery/DS'
 AAF_APP_ID = 'https://example.intersect.org.au/shibboleth'
