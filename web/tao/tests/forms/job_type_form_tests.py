9ed22d32828c692e8b81ed2445069b40741d3a29
Alistair Grant <akgrant@gmail.com>
2013-11-11 14:12:01 +1100

    ASVO-665: Modify automated tests for 0 based index

--- web/tao/tests/forms/job_type_form_tests.py
+++ web/tao/tests/forms/job_type_form_tests.py
@@ -99,12 +99,11 @@ class JobTypeFormTests(LiveServerTest):
         self.upload_params_file()
 
         self.click('tao-tabs-mock_image')
-
         self.assertEqual([u'ALL', 0, 1, 2], self.get_ko_array('catalogue.vm.mock_image.sub_cone_options()', 'value'))
         self.assertEqual([u'1_absolute', u'3_apparent'], self.get_ko_array('catalogue.modules.mock_image.vm.image_settings()[0].mag_field_options()', 'pk'))
         self.assertEqual([u'FITS'], self.get_ko_array('catalogue.vm.mock_image.format_options', 'value'))
 
-        self.assertEqual(2, self.get_image_setting_ko_field(0,'sub_cone'))
+        self.assertEqual(1, self.get_image_setting_ko_field(0,'sub_cone'))
         self.assertEqual('3_apparent', self.get_image_setting_ko_field(0, 'mag_field', field='pk'))
 
         self.assertEqual('7', self.get_image_setting_ko_value(0, 'min_mag'))
@@ -118,7 +117,7 @@ class JobTypeFormTests(LiveServerTest):
         self.assertEqual('66', self.get_image_setting_ko_value(0, 'width'))
         self.assertEqual('66', self.get_image_setting_ko_value(0, 'height'))
 
-        self.assertEqual(3, self.get_image_setting_ko_field(1,'sub_cone'))
+        self.assertEqual(2, self.get_image_setting_ko_field(1,'sub_cone'))
         self.assertEqual('1_absolute', self.get_image_setting_ko_field(1, 'mag_field', field='pk'))
 
         self.assertEqual('', self.get_image_setting_ko_value(1, 'min_mag'))
