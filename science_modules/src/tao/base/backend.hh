227f9e8cea42163ce915e3b7b9432851dd693e7d
Luke Hodkinson <furious.luke@gmail.com>
2014-01-29 16:24:33 +1100

    Adding doxygen comments.

--- science_modules/src/tao/base/backend.hh
+++ science_modules/src/tao/base/backend.hh
@@ -7,19 +7,50 @@
 
 namespace tao {
 
+   ///
+   /// Base class for TAO backends. A backend defines access to
+   /// data sources for astronomical datasets. For example, the
+   /// datasets could be stored in a database (relational or
+   /// otherwise), in HDF5 files or even simple text files. The
+   /// backends abstract the storage from the processing.
+   ///
    class backend
    {
    public:
 
+      ///
+      /// Construct with a simulation.
+      ///
+      /// @param[in] sim The simulation this backend refers to.
+      ///
       backend( tao::simulation<real_type> const* sim = nullptr );
 
+      ///
+      /// Set the simulation. Each backend is constructed to
+      /// represent a particular dataset, which itself must refer
+      /// to a simulation.
+      ///
+      /// @param[in] sim The simulation this backend refers to.
+      ///
       virtual
       void
       set_simulation( tao::simulation<real_type> const* sim );
 
+      ///
+      /// Get the simulation.
+      ///
+      /// @returns The simulation this backend refers to.
+      ///
       tao::simulation<real_type> const*
       simulation() const;
 
+      ///
+      /// Load the simulation details from the dataset. In most
+      /// circumstances the simulation metadata will be stored
+      /// along with the simulation data.
+      ///
+      /// @returns The loaded simulation.
+      ///
       virtual
       tao::simulation<real_type> const*
       load_simulation() = 0;
