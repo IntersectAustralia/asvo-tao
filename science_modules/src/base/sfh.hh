b8bd3ac981d4bba394e2a82161fdd222e0c7056d
Luke Hodkinson <furious.luke@gmail.com>
2014-06-15 12:39:54 +1000

    Accidentally removed the use of the stored dt.

--- science_modules/src/base/sfh.hh
+++ science_modules/src/base/sfh.hh
@@ -398,6 +398,10 @@ namespace tao {
 	       LOGDLN( "Material start age: ", first_age );
 	       LOGDLN( "Material finish age: ", last_age );
 
+	       // Use the stored dt value instead of the age I calculate.
+	       age_size = _dts[idx];
+	       first_age = last_age + age_size;
+
 	       // Use the star formation rates to compute the new mass
 	       // produced. Bear in mind the rates we expect from the
 	       // database will be solar masses per year.
