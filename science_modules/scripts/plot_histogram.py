8faf870de192391f57b9e28c3a48fa13c823e98a
Luke Hodkinson <furious.luke@gmail.com>
2014-08-06 14:57:51 +1000

    Updating the utility scripts.

--- science_modules/scripts/plot_histogram.py
+++ science_modules/scripts/plot_histogram.py
@@ -11,8 +11,9 @@ parser = argparse.ArgumentParser(description='Plot a histogram.')
 parser.add_argument('filename', help='input filename')
 parser.add_argument('column', type=int, help='column to use')
 parser.add_argument('-s', '--seperator', help='parse seperator')
-parser.add_argument('--xl', dest='min_x', default=None, help='minimum x')
-parser.add_argument('--xu', dest='max_x', default=None, help='maximum x')
+parser.add_argument('--bins', '-b', type=int, default=75, help='number of bins')
+parser.add_argument('--xl', dest='min_x', type=float, default=None, help='minimum x')
+parser.add_argument('--xu', dest='max_x', type=float, default=None, help='maximum x')
 parser.add_argument('--log', type=bool, default=False, help='maximum x')
 args = parser.parse_args()
 
@@ -27,13 +28,13 @@ with open(args.filename, 'r') as in_f:
             continue
         if args.log:
             if val > 0.0:
-                x.append(math.log10(val))
-        else:
+                val = math.log10(val)
+        if (args.min_x is None or val >= args.min_x) and (args.max_x is None or val <= args.max_x):
             x.append(val)
 
 plt.figure(figsize=(12.8,12.8), dpi=80)
-plt.hist(x, bins=75)
-plt.xlim([args.min_x, args.max_x])
+plt.hist(x, bins=args.bins)
+# plt.xlim(args.min_x, args.max_x)
 # plt.title('Mass Errors')
 # plt.xlabel('percentage mass difference')
 plt.ylabel('frequency')
